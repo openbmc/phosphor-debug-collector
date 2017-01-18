20530ea8042a59c7e1eeaee7e47f44e5fc822664
Michael Tritz <mtritz@us.ibm.com>
2017-01-18 16:53:49 -0600

    FFDC script - a few additions based on review
    Change-Id: I62c5fbac692255640a9aed3bde7e4ebc36eaa3f6
    Signed-off-by: Michael Tritz <mtritz@us.ibm.com>

--- ffdc.sh
+++ ffdc.sh
@@ -1,28 +1,40 @@
+#!/bin/bash
+
 # Commands to be outputted into individual files
 # Add more entries here as desired!
 # Format: "File name" "Command"
 declare -a arr=(
-      "Build_info"      "cat /etc/version"
-      "FW_level"        "cat /etc/os-release"
-      "OS"              "uname -a"
-      "Uptime"          "uptime"
-      "Disk_usage"      "df -hT"
+      "Build_info.txt"      "cat /etc/version"
+      "FW_level.txt"        "cat /etc/os-release"
+      "OS.txt"              "uname -a"
+      "Uptime.txt"          "uptime"
+      "Disk_usage.txt"      "df -hT"
 
-      "BMC_proc_list"   "top -n 1 -b"
-      "BMC_journalctl"  "journalctl --no-pager"
-      "BMC_dmesg"       "dmesg"
-      "BMC_procinfo"    "cat /proc/cpuinfo"
-      "BMC_meminfo"     "cat /proc/meminfo"
+      "systemctl_status"    "systemctl status | cat"
+
+      "BMC_proc_list.txt"   "top -n 1 -b"
+      "BMC_journalctl.txt"  "journalctl --no-pager"
+      "BMC_dmesg.txt"       "dmesg"
+      "BMC_procinfo.txt"    "cat /proc/cpuinfo"
+      "BMC_meminfo.txt"     "cat /proc/meminfo"
 )
 
 dir=$"ffdc_$(date +"%Y-%m-%d_%H-%M-%S")"
 mkdir "$dir"
 
+echo -n "Please enter destination folder (/tmp if left blank or invalid): "
+read dest
+if [ ! -d "$dest" ]; then
+  echo "Not a valid destination. Using /tmp instead..."
+  dest="/tmp"
+fi
+
 for ((i=0;i<${#arr[@]};i+=2)); do
+  echo "Collecting ${arr[i]}..."
   ${arr[i+1]} >> "$dir/${arr[i]}"
 done
 
-tar -cf "$dir.tar" "$dir"
-echo "Contents in $dir.tar"
+tar -cf "$dest/$dir.tar" "$dir"
+echo "Contents in $dest/$dir.tar"
 
 rm -r "$dir"
