0debbe93c88f45d6058aebf589b9b7be2223960d
Jayanth Othayoth <ojayanth@in.ibm.com>
2017-06-14 07:17:21 -0500

    Implementation of create interface.
    Both the external and internal Dump managers define a "Create"
    interfaces. This commit implements these.
    
    Change-Id: If857ec6ea7267fd72e9b420e6b44fa68b6abab66
    Signed-off-by: Jayanth Othayoth <ojayanth@in.ibm.com>

--- dump_entry.cpp
+++ dump_entry.cpp
@@ -1,17 +1,25 @@
 #include "dump_entry.hpp"
+#include "dump_manager.hpp"
+
+#include <phosphor-logging/log.hpp>
 
 namespace phosphor
 {
 namespace dump
 {
 
-Entry::Entry(sdbusplus::bus::bus& bus, const char* obj): EntryIfaces(bus, obj)
-{
-}
-
+using namespace phosphor::logging;
 
 void Entry::delete_()
 {
+    //Delete Dump file from Permanent location
+    if (!fs::remove(file))
+    {
+        log<level::INFO>("Dump file doesn't exist.",
+                         entry("Name=%s", file.c_str()));
+    }
+    // Remove Dump entry D-bus object
+    parent.erase(id);
 }
 
 } // namespace dump
--- dump_manager_main.cpp
+++ dump_manager_main.cpp
@@ -1,21 +1,38 @@
-#include <sdbusplus/bus.hpp>
-#include <sdbusplus/server/manager.hpp>
+#include <phosphor-logging/elog-errors.hpp>
+#include "xyz/openbmc_project/Common/error.hpp"
 #include "config.h"
+#include "dump_manager.hpp"
+#include "dump_internal.hpp"
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     auto bus = sdbusplus::bus::new_default();
+    using namespace phosphor::logging;
+    using InternalFailure =
+        sdbusplus::xyz::openbmc_project::Common::Error::InternalFailure;
+
+    sd_event* loop = nullptr;
+    sd_event_default(&loop);
 
     // Add sdbusplus ObjectManager for the 'root' path of the DUMP manager.
     sdbusplus::server::manager::manager objManager(bus, DUMP_OBJPATH);
-
     bus.request_name(DUMP_BUSNAME);
 
-    while(true)
+    try
     {
-        bus.process_discard();
-        bus.wait();
+        phosphor::dump::Manager manager(bus, loop, DUMP_OBJPATH);
+        phosphor::dump::internal::Manager mgr(bus, OBJ_INTERNAL);
+        bus.attach_event(loop, SD_EVENT_PRIORITY_NORMAL);
+        sd_event_loop(loop);
     }
+
+    catch (InternalFailure& e)
+    {
+        commit<InternalFailure>();
+        sd_event_unref(loop);
+        return -1;
+    }
+    sd_event_unref(loop);
 
     return 0;
 }
