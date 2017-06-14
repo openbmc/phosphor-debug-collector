5146f5c0cbcbdc68e72710d48c51ce48f4333cd3
Jayanth Othayoth <ojayanth@in.ibm.com>
2017-06-14 07:17:21 -0500

    Implementation of create interface.
    Both the external and internal Dump managers define a "Create"
    interfaces. This commit implements these.
    
    Change-Id: If857ec6ea7267fd72e9b420e6b44fa68b6abab66
    Signed-off-by: Jayanth Othayoth <ojayanth@in.ibm.com>

---
+++ dump_manager.cpp
@@ -0,0 +1,122 @@
+#include "dump_manager.hpp"
+#include "dump_internal.hpp"
+#include "xyz/openbmc_project/Common/error.hpp"
+#include "config.h"
+
+#include <phosphor-logging/elog-errors.hpp>
+#include <unistd.h>
+
+namespace phosphor
+{
+namespace dump
+{
+
+using namespace sdbusplus::xyz::openbmc_project::Common::Error;
+using namespace phosphor::logging;
+
+namespace internal
+{
+
+void Manager::create(
+    type type,
+    std::vector<std::string> fullPaths)
+{
+    // TODO openbmc/openbmc#1795
+    // Add implementaion of internal create function.
+}
+
+} //namepsace internal
+
+uint32_t Manager::createDump()
+{
+    std::vector<std::string> paths;
+
+    auto dumpId = captureDump(
+                      type::UserRequested, paths);
+    return dumpId;
+}
+
+uint32_t Manager:: captureDump(
+    type type,
+    const std::vector<std::string>& fullPaths)
+{
+    pid_t pid = fork();
+
+    // TODO openbmc/openbmc#1795
+    // Add Dump location info.
+    if (pid == 0)
+    {
+        execl("/usr/bin/ffdc", "ffdc", (char*)0);
+
+        //ffdc script execution is failed.
+        log<level::ERR>("Error occurred during ffdc function execution");
+        elog<InternalFailure>();
+    }
+    else if (pid > 0)
+    {
+        sd_event_source* source = nullptr;
+
+        auto rc = sd_event_add_child(eventLoop,
+                                     &source,
+                                     pid,
+                                     WEXITED | WSTOPPED,
+                                     callback,
+                                     nullptr);
+        if (0 > rc)
+        {
+            // Failed to add to event loop
+            log<level::ERR>("Error occurred during the sd_event_add_child call",
+                            entry("rc=%d", rc));
+            elog<InternalFailure>();
+        }
+    }
+    else
+    {
+        auto error = errno;
+        log<level::ERR>("Error occurred during fork",
+                        entry("ERRNO=%s", strerror(error)));
+        elog<InternalFailure>();
+    }
+
+    return ++lastEntryId;
+}
+
+void Manager::createEntry(const fs::path& file)
+{
+    // TODO openbmc/openbmc#1795
+    // Get Dump ID from Dump file name.
+    // Validate the Dump file name.
+    auto id = lastEntryId;
+
+    //Get Epoch time.
+    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(
+                  std::chrono::system_clock::now().time_since_epoch()).count();
+
+    // Entry Object path.
+    auto objPath =  std::string(OBJ_ENTRY) + '/' +
+                    std::to_string(id);
+
+    auto size = fs::file_size(file);
+
+    entries.insert(std::make_pair(id,
+                                  std::make_unique<Entry>(
+                                      bus,
+                                      objPath.c_str(),
+                                      id,
+                                      ms,
+                                      size,
+                                      file,
+                                      *this)));
+}
+
+void Manager::erase(uint32_t entryId)
+{
+    auto entry = entries.find(entryId);
+    if (entries.end() != entry)
+    {
+        entries.erase(entry);
+    }
+}
+
+} //namespace dump
+} //namespace phosphor
---
+++ dump_manager.hpp
@@ -0,0 +1,110 @@
+#pragma once
+
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/server/object.hpp>
+#include <xyz/openbmc_project/Dump/Create/server.hpp>
+#include <experimental/filesystem>
+
+#include "xyz/openbmc_project/Dump/Internal/Create/server.hpp"
+#include "dump_entry.hpp"
+
+namespace phosphor
+{
+namespace dump
+{
+
+namespace fs = std::experimental::filesystem;
+using type =
+    sdbusplus::xyz::openbmc_project::Dump::Internal::server::Create::Type;
+
+using CreateIface = sdbusplus::server::object::object<
+                    sdbusplus::xyz::openbmc_project::Dump::server::Create>;
+
+/** @class Manager
+ *  @brief OpenBMC Dump  manager implementation.
+ *  @details A concrete implementation for the
+ *  xyz.openbmc_project.Dump.Create DBus API.
+ */
+class Manager : public CreateIface
+{
+    public:
+        Manager() = delete;
+        Manager(const Manager&) = default;
+        Manager& operator=(const Manager&) = delete;
+        Manager(Manager&&) = delete;
+        Manager& operator=(Manager&&) = delete;
+        virtual ~Manager() = default;
+
+        /** @brief Constructor to put object onto bus at a dbus path.
+         *  @param[in] bus - Bus to attach to.
+         *  @param[in] loop - Dump manager sd_event loop.
+         *  @param[in] path - Path to attach at.
+         */
+        Manager(sdbusplus::bus::bus& bus, sd_event* loop, const char* path) :
+            CreateIface(bus, path),
+            bus(bus),
+            eventLoop(loop),
+            lastEntryId(0)
+        {}
+
+        /** @brief Implementation for CreateDump
+         *  Method to create Dump.
+         *
+         *  @return id[uint32_t] - The Dump entry id number.
+         */
+        uint32_t createDump() override;
+
+        /** @brief Create Dump entry d-bus object
+         *  @param[in] fullPath - Full path of the Dump file name
+         */
+        void createEntry(const fs::path& fullPath);
+
+        /**  @brief Capture BMC Dump based on the Dump type.
+          *  @param[in] type - Type of the Dump.
+          *  @param[in] fullPaths - List of absolute paths to the files
+          *             to be included as part of Dump package.
+          *  @return id[uint32_t] - The Dump entry id number.
+          */
+        uint32_t captureDump(
+            type type,
+            const std::vector<std::string>& fullPaths);
+
+        /** @brief Erase specified entry d-bus object
+          *
+          * @param[in] entryId - unique identifier of the entry
+          */
+        void erase(uint32_t entryId);
+
+    private:
+        /** @brief sd_event_add_child callback
+          *
+          *  @param[in] s - event source, floating (unused) in our case
+          *  @param[in] si - signal info
+          *  @param[in] userdata - pointer to Watch object
+          *
+          *  @returns 0 on success, -1 on fail
+          */
+        static int callback(sd_event_source* s,
+                            const siginfo_t* si,
+                            void* userdata)
+        {
+            //No specific action required in
+            //the sd_event_add_child callback.
+            return true;
+        }
+
+        /** @brief sdbusplus DBus bus connection. */
+        sdbusplus::bus::bus& bus;
+
+        /** @brief sdbusplus Dump event loopi */
+        sd_event* eventLoop;
+
+        /** @brief Dump Entry dbus objects map based on entry id */
+        std::map<uint32_t, std::unique_ptr<Entry>> entries;
+
+        /** @brief Id of the last Dump entry */
+        uint32_t lastEntryId;
+};
+
+} // namespace dump
+} // namespace phosphor
