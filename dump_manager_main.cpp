7597c1f82f0ca6c7bd1575f580d0115e939f0f98
Jayanth Othayoth <ojayanth@in.ibm.com>
2017-06-14 08:01:41 -0500

    Add support for user initated BMC Dump
    Added support for creating new dump entry d-bus objects for
    user request dump.
    
    Resolves openbmc/openbmc#1510
    
    Change-Id: I9f53d3135dd4bff6187840c0c53d2a64509808cd
    Signed-off-by: Jayanth Othayoth <ojayanth@in.ibm.com>

--- dump_manager_main.cpp
+++ dump_manager_main.cpp
@@ -1,17 +1,22 @@
+#include <sdbusplus/bus.hpp>
 #include <phosphor-logging/elog-errors.hpp>
+#include <xyz/openbmc_project/Dump/Manager/error.hpp>
 
 #include "xyz/openbmc_project/Common/error.hpp"
 #include "config.h"
 #include "dump_manager.hpp"
 #include "dump_internal.hpp"
+#include "watch.hpp"
 
 int main(int argc, char* argv[])
 {
-    auto bus = sdbusplus::bus::new_default();
     using namespace phosphor::logging;
     using InternalFailure =
         sdbusplus::xyz::openbmc_project::Common::Error::InternalFailure;
+    using InvalidDumpPath =
+        sdbusplus::xyz::openbmc_project::Dump::Manager::Error::InvalidDumpPath;
 
+    auto bus = sdbusplus::bus::new_default();
     sd_event* event = nullptr;
     auto rc = sd_event_default(&event);
     if (rc < 0)
@@ -19,7 +24,7 @@ int main(int argc, char* argv[])
         log<level::ERR>("Error occurred during the sd_event_default",
                         entry("rc=%d", rc));
         report<InternalFailure>();
-        return rc;
+        return -1;
     }
     phosphor::dump::EventPtr eventP{event};
     event = nullptr;
@@ -32,6 +37,11 @@ int main(int argc, char* argv[])
     {
         phosphor::dump::Manager manager(bus, eventP, DUMP_OBJPATH);
         phosphor::dump::internal::Manager mgr(bus, OBJ_INTERNAL);
+        phosphor::dump::notify::Watch watch(eventP.get(),
+                                            std::bind(
+                                                std::mem_fn(
+                                                        &phosphor::dump::Manager::createEntry),
+                                                &manager, std::placeholders::_1));
         bus.attach_event(eventP.get(), SD_EVENT_PRIORITY_NORMAL);
         auto rc = sd_event_loop(eventP.get());
         if (rc < 0)
@@ -45,6 +55,12 @@ int main(int argc, char* argv[])
     catch (InternalFailure& e)
     {
         commit<InternalFailure>();
+        return -1;
+    }
+
+    catch (InvalidDumpPath& e)
+    {
+        commit<InvalidDumpPath>();
         return -1;
     }
 
