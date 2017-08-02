596f8a132ed0961dd3f9617abd18304a8930b2d1
Jayanth Othayoth <ojayanth@in.ibm.com>
2017-08-02 06:24:58 -0500

    Cap the number of dumps
    Calculate number of dumps allowed in a system based on
    individual dump Max size configured in the system.
    
    Algorithm: if ( dump entries + active dumps  less than
    total dump size/maximum dump size( per entry) then set to
    new dump size as maximum dump size. Otherwise return error.
    
    Next patch will provide additional algorithm to cap the
    dump based on actual size of the dump files.
    
    Change-Id: Id8916a31d72f5c2f2f23eaf68062b829b7e4100c
    Signed-off-by: Jayanth Othayoth <ojayanth@in.ibm.com>

--- dump_manager.cpp
+++ dump_manager.cpp
@@ -2,11 +2,13 @@
 #include <sys/inotify.h>
 #include <regex>
 
+#include <phosphor-logging/elog.hpp>
 #include <phosphor-logging/elog-errors.hpp>
 
 #include "dump_manager.hpp"
 #include "dump_internal.hpp"
 #include "xyz/openbmc_project/Common/error.hpp"
+#include "xyz/openbmc_project/Dump/Create/error.hpp"
 #include "config.h"
 
 namespace phosphor
@@ -40,6 +42,16 @@ uint32_t Manager::captureDump(
     Type type,
     const std::vector<std::string>& fullPaths)
 {
+    using namespace sdbusplus::xyz::openbmc_project::Dump::Create::Error;
+    using Reason = xyz::openbmc_project::Dump::Create::QuotaExceeded::REASON;
+
+    //Get Dump size.
+    auto size = getAllowedSize();
+    if (size == 0)
+    {
+        elog<QuotaExceeded>(Reason("Not enough space: Delete old dumps"));
+    }
+
     pid_t pid = fork();
 
     if (pid == 0)
@@ -79,11 +91,17 @@ uint32_t Manager::captureDump(
         elog<InternalFailure>();
     }
 
+    //Increment active dump count.
+    activeDumpCount++;
+
     return ++lastEntryId;
 }
 
 void Manager::createEntry(const fs::path& file)
 {
+    //Decrement the Dump in progress counter.
+    activeDumpCount = (activeDumpCount == 0 ? 0 : activeDumpCount - 1);
+
     //Dump File Name format obmcdump_ID_EPOCHTIME.EXT
     static constexpr auto ID_POS         = 1;
     static constexpr auto EPOCHTIME_POS  = 2;
@@ -147,17 +165,17 @@ void Manager::watchCallback(const UserMap& fileInfo)
         else if ((IN_CREATE == i.second) && fs::is_directory(i.first))
         {
             auto watchObj = std::make_unique<Watch>(
-                                    eventLoop,
-                                    IN_NONBLOCK,
-                                    IN_CLOSE_WRITE,
-                                    EPOLLIN,
-                                    i.first,
-                                    std::bind(
-                                         std::mem_fn(
-                                      &phosphor::dump::Manager::watchCallback),
-                                         this, std::placeholders::_1));
+                                eventLoop,
+                                IN_NONBLOCK,
+                                IN_CLOSE_WRITE,
+                                EPOLLIN,
+                                i.first,
+                                std::bind(
+                                    std::mem_fn(
+                                       &phosphor::dump::Manager::watchCallback),
+                                    this, std::placeholders::_1));
 
-           childWatchMap.emplace(i.first, std::move(watchObj));
+            childWatchMap.emplace(i.first, std::move(watchObj));
         }
 
     }
@@ -197,6 +215,28 @@ void Manager::restore()
             }
         }
     }
+}
+
+uint32_t Manager::getAllowedSize()
+{
+    auto size = 0;
+
+    // Maximum number of dump is based on total dump size
+    // and individual dump Max size configured in the system.
+    // Set the new dump size to max, incase sum of avilable
+    // dump and active dumps is less than maximum number of dumps.
+    //
+    if ((entries.size() + activeDumpCount) <
+        (BMC_DUMP_TOTAL_SIZE / BMC_DUMP_SIZE))
+    {
+        size = BMC_DUMP_SIZE;
+    }
+    else
+    {
+        //Reached to maximum limit
+        size = 0;
+    }
+    return size;
 }
 
 } //namespace dump
