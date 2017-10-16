3d2558f0e8c1b5b94b054d0fbb61dce169755f05
Michael Tritz <mtritz@us.ibm.com>
2017-10-16 16:38:56 -0500

    New tool "elog-report" to report standard errors on demand
    This commit adds a tool to the repo with the ability to throw a
    particular error on command.
    
    This is roughly equivalent to part of the functionality of the
    logging-test tool that currently exists in openbmc/phosphor-logging.
    However, that tool, predominantly intended as a unit test, only has the
    ability to report a set of specialized example errors.
    
    This tool differs because it is able to report typical errors that would
    be encountered in OpenBMC programs; it is intended to be used mostly for
    upcoming automated tests.
    
    Resolves openbmc/openbmc#2416
    
    Change-Id: I48f8637ba97f4ba22f0bddab31c9fcde281a6bb1
    Signed-off-by: Michael Tritz <mtritz@us.ibm.com>

---
+++ tools/elog_report.cpp
@@ -0,0 +1,67 @@
+#include <getopt.h>
+#include <iostream>
+#include <sstream>
+#include "xyz/openbmc_project/Common/error.hpp"
+#include <phosphor-logging/elog-errors.hpp>
+#include <phosphor-logging/log.hpp>
+
+using namespace phosphor::logging;
+using namespace sdbusplus::xyz::openbmc_project::Common::Error;
+
+const char *usage = "Usage: elog-report [OPTION]           \n\n\
+Options:                                                     \n\
+[NONE]                          Default test case.           \n\
+-h, --help                      Display this usage text.     \n\
+-r, --report <string>           Report desired error.      \n\n\
+Valid errors to report:                                      \n\
+InternalFailure                                              \n";
+
+int reportError(const char *text)
+{
+    if (strcmp(text, "InternalFailure") == 0)
+    {
+        report<InternalFailure>();
+        return 0;
+    }
+
+    std::cerr << "Unsupported or unrecognized error.\n" << usage;
+    return 1;
+}
+
+int main(int argc, char *argv[])
+{
+    char arg;
+
+    if (argc == 1)
+    {
+        std::cerr << usage;
+        return 1;
+    }
+
+    static struct option long_options[] =
+    {
+          {"help",    no_argument,       0, 'h'},
+          {"report",  required_argument, 0, 'r'},
+          {0, 0, 0, 0}
+    };
+    int option_index = 0;
+
+    while((arg=getopt_long(argc,argv,"hr:",long_options,&option_index)) != -1)
+    {
+        switch (arg)
+        {
+            case 'r':
+                return reportError(optarg);
+            case 'h':
+                std::cout << usage;
+                return 0;
+            case '?':
+                std::cerr << usage;
+                return 1;
+
+        }
+    }
+
+    return 0;
+
+}
