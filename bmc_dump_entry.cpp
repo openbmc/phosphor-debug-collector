b34f285015b159d9b84d49dede2165e813faa222
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2020-01-29 07:11:08 -0600

    Update dump manager and entry to have multiple types
    Created a base dump entry, who ever implementing a new
    dump type need to inherit from the base dump entry.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: Ie85d393f75ec697a7b02b84d131a4a8c0116c3e7

---
+++ bmc_dump_entry.cpp
@@ -0,0 +1,34 @@
+#include "bmc_dump_entry.hpp"
+
+#include "dump_manager.hpp"
+
+#include <phosphor-logging/log.hpp>
+
+namespace phosphor
+{
+namespace dump
+{
+namespace bmc
+{
+using namespace phosphor::logging;
+
+void Entry::delete_()
+{
+    // Delete Dump file from Permanent location
+    try
+    {
+        fs::remove_all(file.parent_path());
+    }
+    catch (fs::filesystem_error& e)
+    {
+        // Log Error message and continue
+        log<level::ERR>(e.what());
+    }
+
+    // Remove Dump entry D-bus object
+    parent.erase(id);
+}
+
+} // namespace bmc
+} // namespace dump
+} // namespace phosphor
