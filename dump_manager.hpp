4e90d4fe686cc9c8b46577d4613993fe6f6a671a
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2020-01-30 01:48:02 -0600

    Implement Notify method
    This commit implements notify method for notfying the
    creation of a new dump.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: Ieb4fa29965b3a93b3dafa2251f354c44ca12db46

--- dump_manager.hpp
+++ dump_manager.hpp
@@ -5,9 +5,11 @@
 #include "bmc_dump_entry.hpp"
 #include "dump_entry.hpp"
 #include "dump_utils.hpp"
+#include "system_dump_entry.hpp"
 #include "watch.hpp"
 #include "xyz/openbmc_project/Collection/DeleteAll/server.hpp"
 #include "xyz/openbmc_project/Dump/Internal/Create/server.hpp"
+#include "xyz/openbmc_project/Dump/NewDump/server.hpp"
 
 #include <experimental/filesystem>
 #include <sdbusplus/bus.hpp>
@@ -32,7 +34,8 @@ using Type =
 
 using CreateIface = sdbusplus::server::object::object<
     sdbusplus::xyz::openbmc_project::Collection::server::DeleteAll,
-    sdbusplus::xyz::openbmc_project::Dump::server::Create>;
+    sdbusplus::xyz::openbmc_project::Dump::server::Create,
+    sdbusplus::xyz::openbmc_project::Dump::server::NewDump>;
 
 namespace fs = std::experimental::filesystem;
 
@@ -97,6 +100,13 @@ class Manager : public CreateIface
      *        representations.
      */
     void restore();
+
+    /**  @brief Notify the dump manager about creation of a new dump.
+     *  @param[in] dumpType - Type of the Dump.
+     *  @param[in] dumpId - Id from the source of the dump.
+     *  @param[in] size - Size of the dump.
+     */
+    void notify(NewDump::DumpType dumpType, uint32_t dumpId, uint64_t size);
 
   private:
     /** @brief Create Dump entry d-bus object
