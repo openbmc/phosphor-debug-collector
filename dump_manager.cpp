4e4cda365502215e0c04d96791404f06ded009cb
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2020-01-30 01:48:02 -0600

    Implement Notify method
    This commit implements notify method for notfying the
    creation of a new dump.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: Ieb4fa29965b3a93b3dafa2251f354c44ca12db46

--- dump_manager.cpp
+++ dump_manager.cpp
@@ -239,5 +239,28 @@ size_t Manager::getAllowedSize()
     return size;
 }
 
+void Manager::notify(NewDump::DumpType dumpType, uint32_t dumpId, uint64_t size)
+{
+    //get the timestamp
+    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(
+                  std::chrono::system_clock::now().time_since_epoch())
+                  .count();
+    //Get the id
+    auto id = lastEntryId + 1;
+    auto idString = std::to_string(id);
+    try
+    {
+        auto objPath = fs::path(OBJ_ENTRY) / idString;
+        entries.insert(std::make_pair(
+            id,
+            std::make_unique<system::Entry>(bus, objPath.c_str(), id, ms,
+                                    size, dumpId, *this)));
+    }
+    catch (const std::invalid_argument& e)
+    {
+        log<level::ERR>(e.what());
+    }
+}
+
 } // namespace dump
 } // namespace phosphor
