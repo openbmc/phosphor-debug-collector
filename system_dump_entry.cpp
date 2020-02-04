a57136606cca7d4e7a54b75895d6e19f4f29f4ba
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2020-02-04 06:39:11 -0600

    Add call to request to offload dump
    Add call through pldm to request offloading the
    dump from host memory.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: I14fd7fdbab78222043cab8a98edd996fd01669c9

---
+++ system_dump_entry.cpp
@@ -0,0 +1,28 @@
+#include "system_dump_entry.hpp"
+
+#include "dump_manager.hpp"
+#include "pldm_interface.hpp"
+#include "xyz/openbmc_project/Common/error.hpp"
+
+#include <phosphor-logging/elog-errors.hpp>
+#include <phosphor-logging/elog.hpp>
+#include <phosphor-logging/log.hpp>
+
+namespace phosphor
+{
+namespace dump
+{
+namespace system
+{
+using namespace phosphor::logging;
+using namespace sdbusplus::xyz::openbmc_project::Common::Error;
+using namespace phosphor::dump::pldm;
+
+void Entry::initiateOffload()
+{
+    requestOffload(sourceDumpId());
+}
+
+} // namespace system
+} // namespace dump
+} // namespace phosphor
