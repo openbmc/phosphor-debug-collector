797d285670115e955f143ea0fee43d43381261a5
Lakshminarayana R. Kammath <lkammath@in.ibm.com>
2020-04-22 12:29:18 -0500

    Enable the BMC dump offload through NBD
    Change-Id: I5250b41ed162905548a7d522b9dc86533d0a9a83
    Signed-off-by: Lakshminarayana R. Kammath <lkammath@in.ibm.com>

---
+++ dump_offload.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "config.h"
+
+#include "dump_entry.hpp"
+#include "xyz/openbmc_project/Common/error.hpp"
+#include "xyz/openbmc_project/Dump/Create/error.hpp"
+#include "xyz/openbmc_project/Dump/Entry/System/server.hpp"
+
+#include <fcntl.h>
+#include <stdint.h>
+#include <unistd.h>
+
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <phosphor-logging/elog-errors.hpp>
+#include <phosphor-logging/elog.hpp>
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/server/object.hpp>
+
+namespace phosphor
+{
+namespace dump
+{
+namespace offload
+{
+
+namespace fs = std::experimental::filesystem;
+
+/**
+ * @brief Kicks off the instructions to
+ *        start offload of the dump using dbus
+ *
+ * @param[in] file - dump filename with relative path.
+ *
+ **/
+void requestOffload(fs::path file);
+
+} // namespace offload
+} // namespace dump
+} // namespace phosphor
