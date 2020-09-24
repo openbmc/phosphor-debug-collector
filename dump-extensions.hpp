45780249d1032d34c0ee71e5a8d22bfe443a4ef3
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2020-09-24 11:59:42 -0500

    Make OpenPower specific dump an optional feature
    System dump kind of open power dumps will be enabled
    only if enabled in the configuration. BMC dump wil be
    enabled by default.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: If20b4711dfcf02a2a8ea23848f7409576813c832

---
+++ dump-extensions.hpp
@@ -0,0 +1,21 @@
+#include "dump_manager.hpp"
+
+#include <memory>
+#include <vector>
+
+namespace phosphor
+{
+namespace dump
+{
+
+using DumpManagerList = std::vector<std::unique_ptr<phosphor::dump::Manager>>;
+/**
+ * @brief load the dump extensions
+ *
+ * @param[in] bus - Bus to attach to
+ * @param[out] dumpMgrList - list dump manager objects.
+ *
+ */
+void loadExtensions(sdbusplus::bus::bus& bus, DumpManagerList& dumpMgrList);
+} // namespace dump
+} // namespace phosphor
