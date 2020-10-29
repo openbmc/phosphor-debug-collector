8f37c53e2d6620c699ccd626bdd9fd4db0f293a0
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2020-10-29 15:29:10 -0500

    Progress tracking support for dump entries.
    A new attribute status is added which indicates the status of the
    dump creation. The user requested dump entries will be created
    with InProgress and updated to Completed once the dump creation
    is completed
    
    Test: Request user initiated BMC and system dump and
    make sure the progress is changing and the dump details
    are updated correctly
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: I68e25c865765dff97913af00bf89c8c3a4b65d43

--- dump-extensions/openpower-dumps/dump_manager_system.cpp
+++ dump-extensions/openpower-dumps/dump_manager_system.cpp
@@ -57,8 +57,9 @@ void Manager::notify(NewDump::DumpType dumpType, uint32_t dumpId, uint64_t size)
     try
     {
         entries.insert(std::make_pair(
-            id, std::make_unique<system::Entry>(bus, objPath.c_str(), id, ms,
-                                                size, dumpId, *this)));
+            id, std::make_unique<system::Entry>(
+                    bus, objPath.c_str(), id, ms, size, dumpId,
+                    phosphor::dump::OperationStatus::Completed, *this)));
     }
     catch (const std::invalid_argument& e)
     {
@@ -90,12 +91,14 @@ sdbusplus::message::object_path Manager::createDump()
     auto id = lastEntryId + 1;
     auto idString = std::to_string(id);
     auto objPath = fs::path(baseEntryPath) / idString;
+    std::time_t timeStamp = std::time(nullptr);
 
     try
     {
         entries.insert(std::make_pair(
-            id, std::make_unique<system::Entry>(bus, objPath.c_str(), id, 0, 0,
-                                                INVALID_SOURCE_ID, *this)));
+            id, std::make_unique<system::Entry>(
+                    bus, objPath.c_str(), id, timeStamp, 0, INVALID_SOURCE_ID,
+                    phosphor::dump::OperationStatus::InProgress, *this)));
     }
     catch (const std::invalid_argument& e)
     {
