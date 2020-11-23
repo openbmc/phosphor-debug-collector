19656b754aece7fbc3d262c0e3a7b60d20279479
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2020-11-22 21:24:30 -0600

    Add Resource dump support
    Add support for resource dump operations like
    creation and offloading.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: Ifeec5670a7806d7ee1b572158b4fff7dc14729a6

---
+++ dump-extensions/openpower-dumps/resource_dump_entry.cpp
@@ -0,0 +1,54 @@
+#include "resource_dump_entry.hpp"
+
+#include "dump_utils.hpp"
+#include "host_transport_exts.hpp"
+
+#include <phosphor-logging/elog-errors.hpp>
+#include <xyz/openbmc_project/Common/error.hpp>
+
+namespace phosphor
+{
+namespace dump
+{
+namespace resource
+{
+using namespace phosphor::logging;
+
+void Entry::initiateOffload(std::string uri)
+{
+    using NotAllowed =
+        sdbusplus::xyz::openbmc_project::Common::Error::NotAllowed;
+    using Reason = xyz::openbmc_project::Common::NotAllowed::REASON;
+
+    BootProgress bootProgressStatus = phosphor::dump::getBootProgress();
+    if ((bootProgressStatus != BootProgress::SystemInitComplete) &&
+        (bootProgressStatus != BootProgress::OSStart) &&
+        (bootProgressStatus != BootProgress::OSRunning))
+    {
+        elog<NotAllowed>(Reason("This dump can be offloaded only when the host is up"));
+        return;
+    }
+    phosphor::dump::Entry::initiateOffload(uri);
+    phosphor::dump::host::requestOffload(sourceDumpId());
+}
+
+void Entry::delete_()
+{
+    auto srcDumpID = sourceDumpId();
+
+    // Remove Dump entry D-bus object
+    phosphor::dump::Entry::delete_();
+
+    // Remove host system dump when host is up by using source dump id
+    // which is present in system dump entry dbus object as a property.
+    BootProgress bootProgressStatus = phosphor::dump::getBootProgress();
+    if ((bootProgressStatus == BootProgress::SystemInitComplete) ||
+        (bootProgressStatus == BootProgress::OSStart) ||
+        (bootProgressStatus == BootProgress::OSRunning))
+    {
+        phosphor::dump::host::requestDelete(srcDumpID);
+    }
+}
+} // namespace resource
+} // namespace dump
+} // namespace phosphor
