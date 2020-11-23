53f02d28d91dbff65ebc901819469185ce88d79a
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2020-11-22 21:24:30 -0600

    Add Resource dump support
    Add support for resource dump operations like
    creation and offloading.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: Ifeec5670a7806d7ee1b572158b4fff7dc14729a6

---
+++ dump-extensions/openpower-dumps/resource_dump_entry.cpp
@@ -0,0 +1,35 @@
+#include "resource_dump_entry.hpp"
+
+#include "dump_utils.hpp"
+#include "host_transport_exts.hpp"
+
+#include <phosphor-logging/elog-errors.hpp>
+#include <xyz/openbmc_project/Common/error.hpp>
+
+namespace phosphor
+{
+namespace dump
+{
+namespace resource
+{
+using namespace phosphor::logging;
+
+void Entry::initiateOffload(std::string uri)
+{
+    using NotAllowed =
+        sdbusplus::xyz::openbmc_project::Common::Error::NotAllowed;
+    using Reason = xyz::openbmc_project::Common::NotAllowed::REASON;
+
+    if (!phosphor::dump:isHostRunning())
+    {
+        elog<NotAllowed>(
+            Reason("This dump can be offloaded only when the host is up"));
+        return;
+    }
+    phosphor::dump::Entry::initiateOffload(uri);
+    phosphor::dump::host::requestOffload(sourceDumpId());
+}
+
+} // namespace resource
+} // namespace dump
+} // namespace phosphor
