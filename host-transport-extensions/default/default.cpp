11cd9f0585d46369c6e955f919bc6c89febebc51
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2020-12-18 02:07:22 -0600

    Add support for resource dump delete.
    Add support for deleting resource dumps in host memory.
    Change requestDelete to accept dump type.
    Update system dump delete to pass dump type.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: I8dcf85a2b38da15ff8451003c11232e340c4ca89

--- host-transport-extensions/default/default.cpp
+++ host-transport-extensions/default/default.cpp
@@ -7,12 +7,13 @@ namespace dump
 {
 namespace host
 {
+
 void requestOffload(uint32_t)
 {
     throw std::runtime_error("Hostdump offload method not specified");
 }
 
-void requestDelete(uint32_t)
+void requestDelete(uint32_t, uint32_t)
 {
     throw std::runtime_error("Hostdump delete method not specified");
 }
