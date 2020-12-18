8900a1d7798122dfc3d24963d361f4e8e60e621d
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2020-12-17 22:24:37 -0600

    Allow initiating or offloading dumps only when host is up.
    When the host is not up the system dump cannot be initiated or
    offloaded. Adding a check whether the host is up before starting
    the creation of the dump.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: Ieb3163ef9b329d3b99807722bd5ac985bf3da25e

--- dump-extensions/openpower-dumps/system_dump_entry.cpp
+++ dump-extensions/openpower-dumps/system_dump_entry.cpp
@@ -3,15 +3,33 @@
 #include "dump_utils.hpp"
 #include "host_transport_exts.hpp"
 
+#include <phosphor-logging/elog-errors.hpp>
+#include <xyz/openbmc_project/Common/error.hpp>
+
 namespace phosphor
 {
 namespace dump
 {
 namespace system
 {
+using namespace phosphor::logging;
 
 void Entry::initiateOffload(std::string uri)
 {
+    using NotAllowed =
+        sdbusplus::xyz::openbmc_project::Common::Error::NotAllowed;
+    using Reason = xyz::openbmc_project::Common::NotAllowed::REASON;
+
+    // Allow offloading only when the host is up.
+    BootProgress bootProgressStatus = phosphor::dump::getBootProgress();
+    if ((bootProgressStatus != BootProgress::SystemInitComplete) &&
+        (bootProgressStatus != BootProgress::OSStart) &&
+        (bootProgressStatus != BootProgress::OSRunning))
+    {
+        elog<NotAllowed>(
+            Reason("System dump can be offloaded only when the host is up"));
+        return;
+    }
     phosphor::dump::Entry::initiateOffload(uri);
     phosphor::dump::host::requestOffload(sourceDumpId());
 }
