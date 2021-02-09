044e7421ae6a6587ba068593d2172dd4c64073b1
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2021-02-09 13:41:24 -0600

    Modify phosphor-dump-monitor to enable multiple file watching
    The current phosphor-dump-monitor is watching core files.
    Refactoring it to watch for files for different kinds of
    dumps.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: Iebfd91b6c58ef0e0babc760ec8efeab1219d2c51

--- core_manager.hpp
+++ core_manager.hpp
@@ -3,6 +3,7 @@
 #include "config.h"
 
 #include "dump_utils.hpp"
+#include "file_watch_manager.hpp"
 #include "watch.hpp"
 
 #include <map>
@@ -13,26 +14,11 @@ namespace dump
 {
 namespace core
 {
-using Watch = phosphor::dump::inotify::Watch;
-using UserMap = phosphor::dump::inotify::UserMap;
-
-/** workaround: Watches for IN_CLOSE_WRITE event for the
- *  jffs filesystem based systemd-coredump core path
- *  Refer openbmc/issues/#2287 for more details.
- *
- *  JFFS_CORE_FILE_WORKAROUND will be enabled for jffs and
- *  for other file system it will be disabled.
- */
-#ifdef JFFS_CORE_FILE_WORKAROUND
-static constexpr auto coreFileEvent = IN_CLOSE_WRITE;
-#else
-static constexpr auto coreFileEvent = IN_CREATE;
-#endif
 
 /** @class Manager
  *  @brief OpenBMC Core manager implementation.
  */
-class Manager
+class Manager : public phosphor::dump::filewatch::Manager
 {
   public:
     Manager() = delete;
@@ -46,31 +32,17 @@ class Manager
      *  @param[in] event - Dump manager sd_event loop.
      */
     Manager(const EventPtr& event) :
-        eventLoop(event.get()),
-        coreWatch(eventLoop, IN_NONBLOCK, coreFileEvent, EPOLLIN, CORE_FILE_DIR,
-                  std::bind(std::mem_fn(
-                                &phosphor::dump::core::Manager::watchCallback),
-                            this, std::placeholders::_1))
+        phosphor::dump::filewatch::Manager(
+            event, CORE_FILE_DIR,
+            "xyz.openbmc_project.Dump.Internal.Create.Type.ApplicationCored")
     {
     }
 
   private:
-    /** @brief Helper function for initiating dump request using
-     *         D-bus internal create interface.
-     *  @param [in] files - Core files list
-     */
-    void createHelper(const std::vector<std::string>& files);
-
     /** @brief Implementation of core watch call back
      * @param [in] fileInfo - map of file info  path:event
      */
-    void watchCallback(const UserMap& fileInfo);
-
-    /** @brief sdbusplus Dump event loop */
-    EventPtr eventLoop;
-
-    /** @brief Core watch object */
-    Watch coreWatch;
+    void watchCallback(const filewatch::UserMap& fileInfo);
 };
 
 } // namespace core
