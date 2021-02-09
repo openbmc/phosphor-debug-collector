0f68412b2534826a1a99478aecd1e0c04393e5b2
George Liu <liuxiwei@inspur.com>
2021-02-09 15:01:53 +0800

    Collect ramoops data into BMC dump
    - Add ramoops file monitor and support to enable ramoops data
      collection for kernel dump.
    
    - Systemd will collect ramoops data and write it into
      /var/lib/systemd/pstore when a system kernel oops/panics.
    
    - Today, need to grab everything in that directory, put it in a dump
      and then delete everything in that directory.
    
    Tested:
    - Simulate a kernel panic by creating similar data in the expected
      location.
      `dmesg > /var/lib/systemd/pstore/dmesg1.txt`
      `dmesg > /var/lib/systemd/pstore/dmesg2.txt`
      `dmesg > /var/lib/systemd/pstore/dmesg3.txt`
      `dmesg > /var/lib/systemd/pstore/dmesg4.txt`
    
      tar -tvf obmcdump_1_248.tar.xz:
    
      drwxr-xr-x 0/0         0 1970-01-01 00:01:32 obmcdump_1_248/
      -rw-r--r-- 0/0     21239 1970-01-01 00:01:32 obmcdump_1_248/dmesg1.txt
      -rw-r--r-- 0/0     21239 1970-01-01 00:01:32 obmcdump_1_248/dmesg2.txt
      -rw-r--r-- 0/0     21239 1970-01-01 00:01:32 obmcdump_1_248/dmesg3.txt
      -rw-r--r-- 0/0     21239 1970-01-01 00:01:32 obmcdump_1_248/dmesg4.txt
      -rw-r--r-- 0/0       162 1970-01-01 00:01:32 obmcdump_1_248/dreport.log
      -rw-r--r-- 0/0       294 1970-01-01 00:01:31 obmcdump_1_248/os-release
      -rw-r--r-- 0/0       278 1970-01-01 00:01:31 obmcdump_1_248/summary.log
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: I05bca408f4dcc2b62350104a0c5f757d740dde22

---
+++ ramoops_manager.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "config.h"
+
+#include <phosphor-logging/log.hpp>
+
+#include <filesystem>
+#include <string>
+#include <vector>
+
+namespace fs = std::filesystem;
+using namespace phosphor::logging;
+
+namespace phosphor
+{
+namespace dump
+{
+namespace ramoops
+{
+
+/** @class Manager
+ *  @brief OpenBMC Core manager implementation.
+ */
+class Manager
+{
+  public:
+    Manager() = delete;
+    Manager(const Manager&) = default;
+    Manager& operator=(const Manager&) = delete;
+    Manager(Manager&&) = delete;
+    Manager& operator=(Manager&&) = delete;
+    virtual ~Manager() = default;
+
+    /** @brief Constructor to create ramoops
+     *  @param[in] filePath - Path where the ramoops are stored.
+     */
+    Manager(const std::string& filePath);
+
+  private:
+    /** @brief Helper function for initiating dump request using
+     *         D-bus internal create interface.
+     *  @param [in] files - ramoops files list
+     */
+    void createHelper(const std::vector<std::string>& files);
+};
+
+} // namespace ramoops
+} // namespace dump
+} // namespace phosphor
