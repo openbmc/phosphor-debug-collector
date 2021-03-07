b2c67003434b97743a4b65080e4c02d0d28d2f73
Dhruvaraj Subhshchandran <dhruvaraj@in.ibm.com>
2021-03-07 13:11:38 -0600

    Add support for Hostboot dump.
    Hostboot dump is a dump created during the boot failure.
    The dump get extracted from SBE with the openpower debug
    collector application.
    That application will call into phosphor dump maanger
    to create a dump with the data collected from each
    SBE in the system.
    The hostboot dump manager will collect each file
    from the predefined location and package it to a dump.
    Unit Test:
    - Create a dump entry with busctl
    - create dummy file in search path
    - Call notify with busctl
    
    Signed-off-by: Dhruvaraj Subhshchandran <dhruvaraj@in.ibm.com>
    Change-Id: I2dab3b7ca976ed33184f770722cca00b1b02c075

---
+++ dump-extensions/openpower-dumps/dump_manager_hostboot.hpp
@@ -0,0 +1,151 @@
+#pragma once
+
+#include "dump_manager.hpp"
+#include "dump_utils.hpp"
+#include "watch.hpp"
+#include "xyz/openbmc_project/Dump/NewDump/server.hpp"
+
+#include <com/ibm/Dump/Create/server.hpp>
+#include <experimental/filesystem>
+#include <xyz/openbmc_project/Dump/Create/server.hpp>
+
+namespace openpower
+{
+namespace dump
+{
+namespace hostboot
+{
+
+using CreateIface = sdbusplus::server::object::object<
+    sdbusplus::xyz::openbmc_project::Dump::server::Create,
+    sdbusplus::com::ibm::Dump::server::Create,
+    sdbusplus::xyz::openbmc_project::Dump::server::NewDump>;
+
+using UserMap = phosphor::dump::inotify::UserMap;
+
+namespace fs = std::experimental::filesystem;
+
+using Watch = phosphor::dump::inotify::Watch;
+
+/** @class Manager
+ *  @brief Hostboot Dump  manager implementation.
+ *  @details A concrete implementation for the
+ *  xyz.openbmc_project.Dump.Create DBus API
+ */
+class Manager : virtual public CreateIface,
+                virtual public phosphor::dump::Manager
+{
+  public:
+    Manager() = delete;
+    Manager(const Manager&) = default;
+    Manager& operator=(const Manager&) = delete;
+    Manager(Manager&&) = delete;
+    Manager& operator=(Manager&&) = delete;
+    virtual ~Manager() = default;
+
+    /** @brief Constructor to put object onto bus at a dbus path.
+     *  @param[in] bus - Bus to attach to.
+     *  @param[in] event - Dump manager sd_event loop.
+     *  @param[in] path - Path to attach at.
+     *  @param[in] baseEntryPath - Base path for dump entry.
+     *  @param[in] filePath - Path where the dumps are stored.
+     */
+    Manager(sdbusplus::bus::bus& bus, const phosphor::dump::EventPtr& event,
+            const char* path, const std::string& baseEntryPath,
+            const char* filePath) :
+        CreateIface(bus, path),
+        phosphor::dump::Manager(bus, path, baseEntryPath),
+        eventLoop(event.get()),
+        dumpWatch(
+            eventLoop, IN_NONBLOCK, IN_CLOSE_WRITE | IN_CREATE, EPOLLIN,
+            filePath,
+            std::bind(
+                std::mem_fn(&openpower::dump::hostboot::Manager::watchCallback),
+                this, std::placeholders::_1)),
+        dumpDir(filePath)
+    {
+    }
+
+    /** @brief Implementation of dump watch call back
+     *  @param [in] fileInfo - map of file info  path:event
+     */
+    void watchCallback(const UserMap& fileInfo);
+
+    /** @brief Construct dump d-bus objects from their persisted
+     *        representations.
+     */
+    void restore() override;
+
+    /** @brief Implementation for CreateDump
+     *  Method to create a Hostboot dump entry when user requests for a
+     *  new Hostboot dump
+     *
+     *  @return object_path - The object path of the new dump entry.
+     */
+    sdbusplus::message::object_path
+        createDump(std::map<std::string, std::string> params) override;
+
+    /** @brief Notify the Hostboot dump manager about creation of a new dump.
+     *  @param[in] dumpId - Id from the source of the dump.
+     *  @param[in] size - Size of the dump.
+     */
+    void notify(uint32_t dumpId, uint64_t size) override;
+
+  private:
+    /** @brief Create Dump entry d-bus object
+     *  @param[in] fullPath - Full path of the Dump file name
+     */
+    void createEntry(const fs::path& fullPath);
+
+    /** @brief Capture a Hostboot Dump based.
+     *  @param[in] fullPaths - List of absolute paths to the files
+     *             to be included as part of Dump package.
+     *  @return id - The Dump entry id number.
+     */
+    uint32_t captureDump(const std::vector<std::string>& fullPaths,
+                         uint32_t dumpId);
+
+    /** @brief sd_event_add_child callback
+     *
+     *  @param[in] s - event source
+     *  @param[in] si - signal info
+     *  @param[in] userdata - pointer to Watch object
+     *
+     *  @returns 0 on success, -1 on fail
+     */
+    static int callback(sd_event_source*, const siginfo_t*, void*)
+    {
+        // No specific action required in
+        // the sd_event_add_child callback.
+        return 0;
+    }
+    /** @brief Remove specified watch object pointer from the
+     *        watch map and associated entry from the map.
+     *        @param[in] path - unique identifier of the map
+     */
+    void removeWatch(const fs::path& path);
+
+    /** @brief Calculate per dump allowed size based on the available
+     *        size in the dump location.
+     *  @returns dump size in kilobytes.
+     */
+    size_t getAllowedSize();
+
+    /** @brief sdbusplus Dump event loop */
+    phosphor::dump::EventPtr eventLoop;
+
+    /** @brief Dump main watch object */
+    Watch dumpWatch;
+
+    /** @brief Path to the dump file*/
+    std::string dumpDir;
+
+    /** @brief Child directory path and its associated watch object map
+     *        [path:watch object]
+     */
+    std::map<fs::path, std::unique_ptr<Watch>> childWatchMap;
+};
+
+} // namespace hostboot
+} // namespace dump
+} // namespace openpower
