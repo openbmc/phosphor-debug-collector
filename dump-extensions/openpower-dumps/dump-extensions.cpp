fd204dcd5f25d2db5c7d739743465d4fbab902b1
Dhruvaraj Subhshchandran <dhruvaraj@in.ibm.com>
2021-03-07 13:11:38 -0600

    Add support for Hostboot dump.
    Hostboot dump is a dump created during the boot failure.
    The dump get extracted from SBE with the openpower debug
    collector application.
    That application will call into phosphor dump maanger
    to create a dump with the data collected from each
    SBE in the system.
    The hostboot dump manager will collect each file
    from the predefined location and package it to a dump.
    Unit Test:
    - Create a dump entry with busctl
    - create dummy file in search path
    - Call notify with busctl
    
    Signed-off-by: Dhruvaraj Subhshchandran <dhruvaraj@in.ibm.com>
    Change-Id: I2dab3b7ca976ed33184f770722cca00b1b02c075

--- dump-extensions/openpower-dumps/dump-extensions.cpp
+++ dump-extensions/openpower-dumps/dump-extensions.cpp
@@ -4,8 +4,15 @@
 
 #include "dump-extensions/openpower-dumps/openpower_dumps_config.h"
 
+#include "dump_manager_hostboot.hpp"
 #include "dump_manager_resource.hpp"
 #include "dump_manager_system.hpp"
+#include "xyz/openbmc_project/Common/error.hpp"
+
+#include <fmt/core.h>
+
+#include <phosphor-logging/elog-errors.hpp>
+#include <phosphor-logging/log.hpp>
 
 namespace phosphor
 {
@@ -14,11 +21,30 @@ namespace dump
 
 void loadExtensions(sdbusplus::bus::bus& bus, DumpManagerList& dumpList)
 {
+    using namespace phosphor::logging;
+    using InternalFailure =
+        sdbusplus::xyz::openbmc_project::Common::Error::InternalFailure;
 
     dumpList.push_back(std::make_unique<openpower::dump::system::Manager>(
         bus, SYSTEM_DUMP_OBJPATH, SYSTEM_DUMP_OBJ_ENTRY));
     dumpList.push_back(std::make_unique<openpower::dump::resource::Manager>(
         bus, RESOURCE_DUMP_OBJPATH, RESOURCE_DUMP_OBJ_ENTRY));
+
+    sd_event* event = nullptr;
+    auto rc = sd_event_default(&event);
+    if (rc < 0)
+    {
+        log<level::ERR>(
+            fmt::format("Error occurred during the sd_event_loop, rc({})", rc)
+                .c_str());
+        elog<InternalFailure>();
+        return;
+    }
+    phosphor::dump::EventPtr eventP{event};
+    event = nullptr;
+    dumpList.push_back(std::make_unique<openpower::dump::hostboot::Manager>(
+        bus, eventP, HOSTBOOT_DUMP_OBJPATH, HOSTBOOT_DUMP_OBJ_ENTRY,
+        HOSTBOOT_DUMP_PATH));
 }
 } // namespace dump
 } // namespace phosphor
