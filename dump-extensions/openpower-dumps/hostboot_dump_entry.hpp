f96e4a454e93dec9239246f16d0a08f069fe52f1
Dhruvaraj Subhshchandran <dhruvaraj@in.ibm.com>
2021-03-07 13:11:38 -0600

    Add support for Hostboot dump.
    Hostboot dump is a dump created during the boot failure.
    The dump get extracted from SBE with the openpower debug
    collector application.
    That application will call into phosphor dump maanger
    to create a dump with the data collected from each
    SBE in the system.
    The hostboot dump manager will collect each file
    from the predefined location and package it to a dump.
    Unit Test:
    - Create a dump entry with busctl
    - create dummy file in search path
    - Call notify with busctl
    
    Signed-off-by: Dhruvaraj Subhshchandran <dhruvaraj@in.ibm.com>
    Change-Id: I2dab3b7ca976ed33184f770722cca00b1b02c075

---
+++ dump-extensions/openpower-dumps/hostboot_dump_entry.hpp
@@ -0,0 +1,101 @@
+#pragma once
+
+#include "com/ibm/Dump/Entry/Hostboot/server.hpp"
+#include "dump_entry.hpp"
+#include "xyz/openbmc_project/Dump/Entry/server.hpp"
+#include "xyz/openbmc_project/Object/Delete/server.hpp"
+#include "xyz/openbmc_project/Time/EpochTime/server.hpp"
+
+#include <experimental/filesystem>
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/server/object.hpp>
+
+namespace openpower
+{
+namespace dump
+{
+namespace hostboot
+{
+template <typename T>
+using ServerObject = typename sdbusplus::server::object::object<T>;
+
+using EntryIfaces = sdbusplus::server::object::object<
+    sdbusplus::com::ibm::Dump::Entry::server::Hostboot>;
+
+namespace fs = std::experimental::filesystem;
+
+class Manager;
+
+/** @class Entry
+ *  @brief Hostboot Dump Entry implementation.
+ *  @details A concrete implementation for the
+ *  xyz.openbmc_project.Dump.Entry DBus API
+ */
+class Entry : virtual public EntryIfaces, virtual public phosphor::dump::Entry
+{
+  public:
+    Entry() = delete;
+    Entry(const Entry&) = delete;
+    Entry& operator=(const Entry&) = delete;
+    Entry(Entry&&) = delete;
+    Entry& operator=(Entry&&) = delete;
+    ~Entry() = default;
+
+    /** @brief Constructor for the Dump Entry Object
+     *  @param[in] bus - Bus to attach to.
+     *  @param[in] objPath - Object path to attach to
+     *  @param[in] dumpId - Dump id.
+     *  @param[in] timeStamp - Dump creation timestamp
+     *             since the epoch.
+     *  @param[in] fileSize - Dump file size in bytes.
+     *  @param[in] file - Name of dump file.
+     *  @param[in] status - status of the dump.
+     *  @param[in] parent - The dump entry's parent.
+     */
+    Entry(sdbusplus::bus::bus& bus, const std::string& objPath, uint32_t dumpId,
+          uint64_t timeStamp, uint64_t fileSize, const fs::path& file,
+          phosphor::dump::OperationStatus status,
+          phosphor::dump::Manager& parent) :
+        EntryIfaces(bus, objPath.c_str(), true),
+        phosphor::dump::Entry(bus, objPath.c_str(), dumpId, timeStamp, fileSize,
+                              status, parent),
+        file(file)
+    {
+        // Emit deferred signal.
+        this->openpower::dump::hostboot::EntryIfaces::emit_object_added();
+    }
+
+    /** @brief Delete this d-bus object.
+     */
+    void delete_() override;
+
+    /** @brief Method to initiate the offload of dump
+     *  @param[in] uri - URI to offload dump
+     */
+    void initiateOffload(std::string uri) override;
+
+    /** @brief Method to update an existing dump entry, once the dump creation
+     *  is completed this function will be used to update the entry which got
+     *  created during the dump request.
+     *  @param[in] timeStamp - Dump creation timestamp
+     *  @param[in] fileSize - Dump file size in bytes.
+     *  @param[in] file - Name of dump file.
+     */
+    void update(uint64_t timeStamp, uint64_t fileSize, const fs::path& filePath)
+    {
+        elapsed(timeStamp);
+        size(fileSize);
+        // TODO: Handled dump failed case with #ibm-openbmc/2808
+        status(OperationStatus::Completed);
+        file = filePath;
+        completedTime(timeStamp);
+    }
+
+  private:
+    /** @Dump file name */
+    fs::path file;
+};
+
+} // namespace hostboot
+} // namespace dump
+} // namespace openpower
