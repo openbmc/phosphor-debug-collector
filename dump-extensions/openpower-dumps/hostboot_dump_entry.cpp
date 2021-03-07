58a8f8105f1f8033d727f62658dbff4fe1321c46
Dhruvaraj Subhshchandran <dhruvaraj@in.ibm.com>
2021-03-07 13:11:38 -0600

    Add support for Hostboot dump.
    Hostboot dump is a dump created during the boot failure.
    The dump get extracted from SBE with the openpower debug
    collector application.
    That application will call into phosphor dump maanger
    to create a dump with the data collected from each
    SBE in the system.
    The hostboot dump manager will collect each file
    from the predefined location and package it to a dump.
    Unit Test:
    - Create a dump entry with busctl
    - create dummy file in search path
    - Call notify with busctl
    
    Signed-off-by: Dhruvaraj Subhshchandran <dhruvaraj@in.ibm.com>
    Change-Id: I2dab3b7ca976ed33184f770722cca00b1b02c075

---
+++ dump-extensions/openpower-dumps/hostboot_dump_entry.cpp
@@ -0,0 +1,41 @@
+#include "hostboot_dump_entry.hpp"
+
+#include "dump_manager.hpp"
+#include "dump_offload.hpp"
+
+#include <phosphor-logging/log.hpp>
+
+namespace openpower
+{
+namespace dump
+{
+namespace hostboot
+{
+using namespace phosphor::logging;
+
+void Entry::delete_()
+{
+    // Delete Dump file from Permanent location
+    try
+    {
+        std::filesystem::remove_all(file.parent_path());
+    }
+    catch (std::filesystem::filesystem_error& e)
+    {
+        // Log Error message and continue
+        log<level::ERR>(e.what());
+    }
+
+    // Remove Dump entry D-bus object
+    phosphor::dump::Entry::delete_();
+}
+
+void Entry::initiateOffload(std::string uri)
+{
+    phosphor::dump::offload::requestOffload(file, id, uri);
+    offloaded(true);
+}
+
+} // namespace hostboot
+} // namespace dump
+} // namespace openpower
