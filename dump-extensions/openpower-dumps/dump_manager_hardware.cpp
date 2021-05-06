cb27d9fe09d9655237166e69f35a5904eaf6ff67
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2021-05-06 08:55:36 -0500

    OpenPOWER: Add support for hardware dump
    Hardware dump is a collection hardware state information,
    including various registers, and it is used for debugging
    system checkstop. checkstop is the descriptive term for
    entire system termination by the hardware due to a detected error.
    
    This commits adds
     - Hardware dump entry implementation
     - Hardware dump manager implementation
    
    Tested:
      - Create a hardware dump
      - Delete a hardware dump
      - Offload hardware dump
      - Restore hardware dumps after restart
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: I7f6b83896b2c78d9eb4295212a62476b1fe1d6c0

---
+++ dump-extensions/openpower-dumps/dump_manager_hardware.cpp
@@ -0,0 +1,97 @@
+#include "config.h"
+
+#include "dump_manager_hardware.hpp"
+
+#include "hardware_dump_entry.hpp"
+#include "xyz/openbmc_project/Common/error.hpp"
+#include "xyz/openbmc_project/Dump/Create/error.hpp"
+
+#include <fmt/core.h>
+#include <sys/inotify.h>
+#include <unistd.h>
+
+#include <phosphor-logging/elog-errors.hpp>
+#include <phosphor-logging/elog.hpp>
+
+#include <ctime>
+#include <regex>
+
+namespace openpower
+{
+namespace dump
+{
+namespace hardware
+{
+
+using namespace sdbusplus::xyz::openbmc_project::Common::Error;
+using namespace phosphor::logging;
+
+constexpr auto INVALID_DUMP_SIZE = 0;
+
+sdbusplus::message::object_path
+    Manager::createDump(phosphor::dump::DumpCreateParams params)
+{
+    if (!params.empty())
+    {
+        log<level::WARNING>("Hardware dump accepts no additional parameters");
+    }
+
+    uint32_t id = ++lastEntryId;
+    // Entry Object path.
+    auto objPath = std::filesystem::path(baseEntryPath) / std::to_string(id);
+
+    std::time_t timeStamp = std::time(nullptr);
+    createEntry(id, objPath, timeStamp, 0, std::string(),
+                phosphor::dump::OperationStatus::InProgress);
+
+    return objPath.string();
+}
+
+void Manager::createEntry(const uint32_t id, const std::string objPath,
+                          const uint64_t ms, uint64_t fileSize,
+                          const std::filesystem::path& file,
+                          phosphor::dump::OperationStatus status)
+{
+    try
+    {
+        entries.insert(std::make_pair(
+            id,
+            std::make_unique<openpower::dump::hardware::Entry>(
+                bus, objPath.c_str(), id, ms, fileSize, file, status, *this)));
+    }
+    catch (const std::invalid_argument& e)
+    {
+        log<level::ERR>(fmt::format("Error in creating hardware dump entry, "
+                                    "errormsg({}), OBJECTPATH({}), ID({})",
+                                    e.what(), objPath.c_str(), id)
+                            .c_str());
+        elog<InternalFailure>();
+    }
+}
+
+void Manager::notify(uint32_t dumpId, uint64_t)
+{
+    // Get Dump size.
+    // TODO #ibm-openbmc/issues/3061
+    // Dump request will be rejected if there is not enough space told
+    // one complete dump, change this behavior to crate a partial dump
+    // with available space.
+    auto size = getAllowedSize();
+    std::vector<std::string> paths;
+    try
+    {
+        util::captureDump(dumpId, allowedSize, HARDWARE_DUMP_TMP_FILE_DIR,
+                          dumpDir, "hwdump", event);
+    }
+    catch (std::exception& e)
+    {
+        log<level::ERR>(
+            "Failed to package hardware dump: dump({id}) errorMsg({})", dumpId,
+            e.what());
+        elog<InternalFailure>();
+    }
+}
+
+} // namespace hardware
+} // namespace dump
+} // namespace openpower
