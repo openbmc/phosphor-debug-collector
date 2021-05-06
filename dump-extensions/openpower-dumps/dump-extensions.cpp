0903211075b3e11504c5858fefa6b2b8022d6658
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2021-05-06 08:55:36 -0500

    OpenPOWER: Add support for hardware dump
    Hardware dump is a collection hardware state information,
    including various registers, and it is used for debugging
    system checkstop. checkstop is the descriptive term for
    entire system termination by the hardware due to a detected error.
    
    This commits adds
     - Hardware dump entry implementation
     - Hardware dump manager implementation
    
    Tested:
      - Create a hardware dump
    busctl --verbose call org.open_power.Dump.Manager /org/openpower/dump xyz.openbmc_project.Dump.Create CreateDump a{sv} 3  "com.ibm.Dump.Create.CreateParameters.DumpType" s "com.ibm.Dump.Create.DumpType.Hardware" "com.ibm.Dump.Create.CreateParameters.ErrorLogId" t 0xDEADBEEF "com.ibm.Dump.Create.CreateParameters.FailingUnitId" t 1
    MESSAGE "o" {
            OBJECT_PATH "/xyz/openbmc_project/dump/hardware/entry/1";
    };
    /var/lib/phosphor-debug-collector/hardwaredump/1/hwdump_1_1627559369.tar.gz
    
      - Delete a hardware dump
    busctl call xyz.openbmc_project.Dump.Manager /xyz/openbmc_project/dump/hardware/entry/1 xyz.openbmc_project.Object.Delete Delete
    
      - Offload hardware dump
      - Restore hardware dumps after restart
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: I7f6b83896b2c78d9eb4295212a62476b1fe1d6c0

--- dump-extensions/openpower-dumps/dump-extensions.cpp
+++ dump-extensions/openpower-dumps/dump-extensions.cpp
@@ -4,6 +4,8 @@
 
 #include "dump-extensions/openpower-dumps/openpower_dumps_config.h"
 
+#include "dump_manager_hardware.hpp"
+#include "dump_manager_hostboot.hpp"
 #include "dump_manager_resource.hpp"
 #include "dump_manager_system.hpp"
 
@@ -19,6 +21,40 @@ void loadExtensions(sdbusplus::bus::bus& bus, DumpManagerList& dumpList)
         bus, SYSTEM_DUMP_OBJPATH, SYSTEM_DUMP_OBJ_ENTRY));
     dumpList.push_back(std::make_unique<openpower::dump::resource::Manager>(
         bus, RESOURCE_DUMP_OBJPATH, RESOURCE_DUMP_OBJ_ENTRY));
+
+    try
+    {
+        std::filesystem::create_directories(HOSTBOOT_DUMP_PATH);
+    }
+    catch (std::exception& e)
+    {
+        log<level::ERR>(
+            fmt::format("Failed to create hostboot dump directory({})",
+                        HOSTBOOT_DUMP_PATH)
+                .c_str());
+        throw std::runtime_error("Failed to create hostboot dump directory");
+    }
+
+    dumpList.push_back(std::make_unique<openpower::dump::hostboot::Manager>(
+        bus, event, HOSTBOOT_DUMP_OBJPATH, HOSTBOOT_DUMP_OBJ_ENTRY,
+        HOSTBOOT_DUMP_PATH));
+
+    try
+    {
+        std::filesystem::create_directories(HARDWARE_DUMP_PATH);
+    }
+    catch (std::exception& e)
+    {
+        log<level::ERR>(
+            fmt::format("Failed to create hardware dump directory({})",
+                        HARDWARE_DUMP_PATH)
+                .c_str());
+        throw std::runtime_error("Failed to create hardware dump directory");
+    }
+
+    dumpList.push_back(std::make_unique<openpower::dump::hardware::Manager>(
+        bus, event, HARDWARE_DUMP_OBJPATH, HARDWARE_DUMP_OBJ_ENTRY,
+        HARDWARE_DUMP_PATH));
 }
 } // namespace dump
 } // namespace phosphor
