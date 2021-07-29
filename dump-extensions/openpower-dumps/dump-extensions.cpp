a86434850c43da26b00867a5056142d8e62c4bc5
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2021-07-29 00:30:38 -0500

    OpenPOWER: Remove directory creation service
    Remove the service file to create directories for packaged
    OpenPOWER dumps, instead of that directories will be
    created before creating the respective dump manager
    objects.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: Ieca5bd35604a3be8f2c7acc2c8b833a73d84da5b

--- dump-extensions/openpower-dumps/dump-extensions.cpp
+++ dump-extensions/openpower-dumps/dump-extensions.cpp
@@ -8,11 +8,9 @@
 #include "dump_manager_resource.hpp"
 #include "dump_manager_system.hpp"
 #include "dump_utils.hpp"
-#include "xyz/openbmc_project/Common/error.hpp"
 
 #include <fmt/core.h>
 
-#include <phosphor-logging/elog-errors.hpp>
 #include <phosphor-logging/log.hpp>
 
 namespace phosphor
@@ -20,6 +18,7 @@ namespace phosphor
 namespace dump
 {
 
+using namespace phosphor::logging;
 void loadExtensions(sdbusplus::bus::bus& bus,
                     const phosphor::dump::EventPtr& event,
                     DumpManagerList& dumpList)
@@ -28,6 +27,19 @@ void loadExtensions(sdbusplus::bus::bus& bus,
         bus, SYSTEM_DUMP_OBJPATH, SYSTEM_DUMP_OBJ_ENTRY));
     dumpList.push_back(std::make_unique<openpower::dump::resource::Manager>(
         bus, RESOURCE_DUMP_OBJPATH, RESOURCE_DUMP_OBJ_ENTRY));
+
+    try
+    {
+        std::filesystem::create_directories(HOSTBOOT_DUMP_PATH);
+    }
+    catch (std::exception& e)
+    {
+        log<level::ERR>(
+            fmt::format("Failed to create hostboot dump directory({})",
+                        HOSTBOOT_DUMP_PATH)
+                .c_str());
+        throw std::runtime_error("Failed to create hostboot dump directory");
+    }
 
     dumpList.push_back(std::make_unique<openpower::dump::hostboot::Manager>(
         bus, event, HOSTBOOT_DUMP_OBJPATH, HOSTBOOT_DUMP_OBJ_ENTRY,
