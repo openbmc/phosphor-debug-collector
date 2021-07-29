943926924ac0d14ba1f3f2d4547a17761883826d
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2021-07-28 22:58:26 -0500

    OpenPOWER: common OpenPOWER dump capture function
    Create a common dump capture function for OpenPOWER dumps
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: I0643905ba2888de9913c2ff65d6da3ac9014f5af

--- dump-extensions/openpower-dumps/op_dump_util.hpp
+++ dump-extensions/openpower-dumps/op_dump_util.hpp
@@ -9,6 +9,7 @@ namespace dump
 namespace util
 {
 
+<<<<<<< HEAD
 /** @brief Check whether OpenPOWER dumps are enabled
  *
  * A xyz.openbmc_project.Dump.Create.Error.Disabled will be thrown
@@ -30,6 +31,21 @@ bool isInMpReboot();
  *          false - No dump in progress
  */
 bool isSystemDumpInProgress();
+
+/** @brief Package an OpenPOWER dump files from a location
+ *  into the dump format with dump header.
+ *  @param[in] dumpId - Id of the dump
+ *  @param[in] allowedSize - Size available for the dump
+ *  @param[in] inputDir - Locations of input dump pieces
+ *  @param[in] packageDir - Location where packaged dump should be stored
+ *  @param[in] dumpPrefix - Dump filename prefix
+ *  @param[in] event - sd_event_loop
+ */
+void captureDump(uint32_t dumpId, size_t allowedSize,
+                 const std::string& inputDir, const std::string& packageDir,
+                 const std::string& dumpPrefix,
+                 const phosphor::dump::EventPtr& event);
+
 } // namespace util
 } // namespace dump
 } // namespace openpower
