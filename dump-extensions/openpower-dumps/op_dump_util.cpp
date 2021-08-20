7ed2988bf44ce77c039adc92b119b2922adbaebb
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2021-08-20 04:06:06 -0500

    OpenPOWER: Add support for OpenPOWER dump policy
    Add support for system dump policy, here system dump
    referes to all OpenPOWER dumps, since all dumps from
    host subsystems on OpenPOWER systems are known as system dumps.
    Currently supported policy is dump enable. The dump will be
    created only if the policy is enabled, if the dump is not
    enabled, a dump disabled error will be returned.
    
    If the settings service is not available, considering as
    dump is enabled.
    
    Tests:
    - Create a dump with policy enabled
    busctl --verbose call xyz.openbmc_project.Settings /xyz/openbmc_project/dump/system_dump_policy org.freedesktop.DBus.Properties Get ss "xyz.openbmc_project.Object.Enable" "Enabled"
    MESSAGE "v" {
            VARIANT "b" {
                    BOOLEAN true;
            };
    };
    
    busctl --verbose call xyz.openbmc_project.Dump.Manager /xyz/openbmc_project/dump/resource  xyz.openbmc_project.Dump.Create CreateDump a{sv} 2 "com.ibm.Dump.Create.CreateParameters.VSPString" s "vsp" "com.ibm.Dump.Create.CreateParameters.Password" s "password"
    MESSAGE "o" {
            OBJECT_PATH "/xyz/openbmc_project/dump/resource/entry/1";
    };
    
    Result: Dump created
    
    - Create a dump with policy disabled
    busctl --verbose call xyz.openbmc_project.Settings /xyz/openbmc_project/dump/system_dump_policy org.freedesktop.DBus.Properties Get ss "xyz.openbmc_project.Object.Enable" "Enabled"
    MESSAGE "v" {
            VARIANT "b" {
                    BOOLEAN false;
            };
    };
    
    busctl --verbose call xyz.openbmc_project.Dump.Manager /xyz/openbmc_project/dump/resource  xyz.openbmc_project.Dump.Create CreateDump a{sv} 2 "com.ibm.Dump.Create.CreateParameters.VSPString" s "vsp" "com.ibm.Dump.Create.CreateParameters.Password" s "password"
    Call failed: Dump is disabled on this system.
    
    Result: Dump is not allowed
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: I5dc609b1f4fd0f36df8520f8a75c18a4cd3d7c4c

---
+++ dump-extensions/openpower-dumps/op_dump_util.cpp
@@ -0,0 +1,67 @@
+#include "op_dump_util.hpp"
+
+#include "xyz/openbmc_project/Common/error.hpp"
+#include "xyz/openbmc_project/Dump/Create/error.hpp"
+
+#include <fmt/core.h>
+#include <unistd.h>
+
+#include <phosphor-logging/elog-errors.hpp>
+#include <phosphor-logging/elog.hpp>
+
+#include <filesystem>
+
+namespace openpower
+{
+namespace dump
+{
+namespace util
+{
+using namespace phosphor::logging;
+using InternalFailure =
+    sdbusplus::xyz::openbmc_project::Common::Error::InternalFailure;
+
+void isOPDumpsEnabled()
+{
+    auto enabled = true;
+    constexpr auto enable = "xyz.openbmc_project.Object.Enable";
+    constexpr auto policy = "/xyz/openbmc_project/dump/system_dump_policy";
+    constexpr auto property = "org.freedesktop.DBus.Properties";
+
+    using disabled =
+        sdbusplus::xyz::openbmc_project::Dump::Create::Error::Disabled;
+
+    try
+    {
+        auto bus = sdbusplus::bus::new_default();
+
+        auto service = phosphor::dump::getService(bus, policy, enable);
+
+        auto method =
+            bus.new_method_call(service.c_str(), policy, property, "Get");
+        method.append(enable, "Enabled");
+        auto reply = bus.call(method);
+        std::variant<bool> v;
+        reply.read(v);
+        enabled = std::get<bool>(v);
+    }
+    catch (const sdbusplus::exception::SdBusError& e)
+    {
+        log<level::ERR>(
+            fmt::format("Error({}) in getting dump policy, default is enabled",
+                        e.what())
+                .c_str());
+        report<InternalFailure>();
+    }
+
+    if (!enabled)
+    {
+        log<level::ERR>("OpePOWER dumps are disabled, skipping");
+        elog<disabled>();
+    }
+    log<level::INFO>("OpenPOWER dumps are enabled");
+}
+
+} // namespace util
+} // namespace dump
+} // namespace openpower
