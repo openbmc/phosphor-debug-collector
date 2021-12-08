99106ab10af2e4b7ce810f790a0ce2fded281631
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2021-12-08 03:48:23 -0600

    Implement GetFileHandle in dump entry interface
    Certain applications need to offload the dump in various
    ways, for example, performing DMA transfer of the dump to
    the host memory. These applications require direct access
    to the file to read from the BMC storage.
    
    This commit introduces the GetFileHandle method in the
    dump entry interface. This method provides direct access
    to the Unix file handle of the dump entry, facilitating
    more direct read operations on the dump file.
    The returned file descriptor is read-only, which ensures
    the integrity of the dump file.
    
    The definition of the file handle is moved from the
    specific 'bmc_dump_entry' class to the parent 'dump_entry'
    class. This allows all dump types inheriting from
    'dump_entry' to use this new method, thus increasing
    its utility across various dump types.
    
    The introduction of this method primarily benefits PLDM
    for offloading the dump to the host, and bmcweb for
    offloading the dump to Redfish clients. By providing
    a file handle instead of exposing the file path,
    it resolves potential access issues for applications
    running in non-root contexts.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: If398940b655b4d90688273f0c76a20d4ead61992

--- dump_entry.cpp
+++ dump_entry.cpp
@@ -2,6 +2,18 @@
 
 #include "dump_manager.hpp"
 
+#include <fcntl.h>
+
+#include <phosphor-logging/elog-errors.hpp>
+#include <phosphor-logging/elog.hpp>
+#include <phosphor-logging/lg2.hpp>
+#include <sdeventplus/event.hpp>
+#include <sdeventplus/source/event.hpp>
+#include <xyz/openbmc_project/Common/File/error.hpp>
+#include <xyz/openbmc_project/Common/error.hpp>
+
+#include <cstring>
+
 namespace phosphor
 {
 namespace dump
@@ -11,6 +23,37 @@ void Entry::delete_()
 {
     // Remove Dump entry D-bus object
     parent.erase(id);
+}
+
+sdbusplus::message::unix_fd Entry::getFileHandle()
+{
+    using namespace sdbusplus::xyz::openbmc_project::Common::File::Error;
+    using metadata = xyz::openbmc_project::Common::File::Open;
+    if (file.empty())
+    {
+        lg2::error("Failed to get file handle: File string is empty.");
+        elog<sdbusplus::xyz::openbmc_project::Common::Error::Unavailable>();
+    }
+
+    int fd = open(file.c_str(), O_RDONLY | O_NONBLOCK);
+    if (fd == -1)
+    {
+        auto err = errno;
+        lg2::error("Failed to open dump file: id: {ID} error: {ERRNO}", "ID",
+                   id, "ERRNO", std::strerror(errno));
+        elog<Open>(metadata::ERRNO(err), metadata::PATH(file.c_str()));
+    }
+
+    // Create a new Defer event source for closing this fd
+    sdeventplus::Event event = sdeventplus::Event::get_default();
+    auto eventSource = std::make_unique<sdeventplus::source::Defer>(
+        event, [this, fd](auto& /*source*/) { closeFD(fd); });
+
+    // Insert the file descriptor and corresponding event source into the
+    // map
+    fdCloseEventSources.emplace(fd, std::move(eventSource));
+
+    return fd;
 }
 
 } // namespace dump
