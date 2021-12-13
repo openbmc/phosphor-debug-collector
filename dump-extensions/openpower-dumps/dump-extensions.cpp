836917e22dc33d4f7570abf64947ae859bbec278
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2021-12-12 22:40:07 -0600

    Update host dump ids
    Host dump ids need to be started from certain numbers
    this commit adds the starting if for each type of dumps
    
    Hardare dump: Starting ID - 0
    Hostboot dump: Starting ID - 20000001
    SBE dump: Starting ID - 30000001
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: I26945184c727e70fb9f543eaa7c2266a650b2793

--- dump-extensions/openpower-dumps/dump-extensions.cpp
+++ dump-extensions/openpower-dumps/dump-extensions.cpp
@@ -46,9 +46,9 @@ void loadExtensions(sdbusplus::bus::bus& bus,
     dumpList.push_back(std::make_unique<openpower::dump::hostdump::Manager<
                            openpower::dump::hostboot::Entry>>(
         bus, event, HOSTBOOT_DUMP_OBJPATH, HOSTBOOT_DUMP_OBJ_ENTRY,
-        HOSTBOOT_DUMP_PATH, "hbdump", HOSTBOOT_DUMP_TMP_FILE_DIR,
-        HOSTBOOT_DUMP_MAX_SIZE, HOSTBOOT_DUMP_MIN_SPACE_REQD,
-        HOSTBOOT_DUMP_TOTAL_SIZE));
+        HOSTBOOT_DUMP_START_ID, HOSTBOOT_DUMP_PATH, "hbdump",
+        HOSTBOOT_DUMP_TMP_FILE_DIR, HOSTBOOT_DUMP_MAX_SIZE,
+        HOSTBOOT_DUMP_MIN_SPACE_REQD, HOSTBOOT_DUMP_TOTAL_SIZE));
 
     try
     {
@@ -66,9 +66,9 @@ void loadExtensions(sdbusplus::bus::bus& bus,
     dumpList.push_back(std::make_unique<openpower::dump::hostdump::Manager<
                            openpower::dump::hardware::Entry>>(
         bus, event, HARDWARE_DUMP_OBJPATH, HARDWARE_DUMP_OBJ_ENTRY,
-        HARDWARE_DUMP_PATH, "hwdump", HARDWARE_DUMP_TMP_FILE_DIR,
-        HARDWARE_DUMP_MAX_SIZE, HARDWARE_DUMP_MIN_SPACE_REQD,
-        HARDWARE_DUMP_TOTAL_SIZE));
+        HARDWARE_DUMP_START_ID, HARDWARE_DUMP_PATH, "hwdump",
+        HARDWARE_DUMP_TMP_FILE_DIR, HARDWARE_DUMP_MAX_SIZE,
+        HARDWARE_DUMP_MIN_SPACE_REQD, HARDWARE_DUMP_TOTAL_SIZE));
 
     try
     {
@@ -85,8 +85,8 @@ void loadExtensions(sdbusplus::bus::bus& bus,
     dumpList.push_back(
         std::make_unique<
             openpower::dump::hostdump::Manager<openpower::dump::sbe::Entry>>(
-            bus, event, SBE_DUMP_OBJPATH, SBE_DUMP_OBJ_ENTRY, SBE_DUMP_PATH,
-            "sbedump", SBE_DUMP_TMP_FILE_DIR, SBE_DUMP_MAX_SIZE,
+            bus, event, SBE_DUMP_OBJPATH, SBE_DUMP_OBJ_ENTRY, SBE_DUMP_START_ID,
+            SBE_DUMP_PATH, "sbedump", SBE_DUMP_TMP_FILE_DIR, SBE_DUMP_MAX_SIZE,
             SBE_DUMP_MIN_SPACE_REQD, SBE_DUMP_TOTAL_SIZE));
 }
 } // namespace dump
