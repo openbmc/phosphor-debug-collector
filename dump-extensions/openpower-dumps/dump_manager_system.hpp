2822a374661638e7dd647ed1f6791d64d4ad6e6e
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2022-01-18 05:54:02 -0600

    Fix for host dump delete
    If the dump is not created by host or the request is not
    complete the dump id provided by the host will not be
    valid. When deleting such dump entries a request for
    delete should not be send to the host.
    
    Tests:
    
    Deleting a dump which is not finished when host is running
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: If7e8b7fad67c215f057152bb64b5232f198fee9e

--- dump-extensions/openpower-dumps/dump_manager_system.hpp
+++ dump-extensions/openpower-dumps/dump_manager_system.hpp
@@ -15,7 +15,6 @@ namespace dump
 namespace system
 {
 
-constexpr uint32_t INVALID_SOURCE_ID = 0xFFFFFFFF;
 using NotifyIface = sdbusplus::server::object::object<
     sdbusplus::xyz::openbmc_project::Dump::server::Create,
     sdbusplus::xyz::openbmc_project::Dump::server::NewDump>;
