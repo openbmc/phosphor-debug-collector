730db14b6d1a563d84a50d22ee04007d0a7c431c
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2022-02-16 08:04:07 -0600

    Report dump packaging error
    If the dump packaging is failed update the status as
    failed.
    
    Tests:
    
    Add error in opdreport and initiated dump
    
    Feb 19 06:24:56 openpower-dump-manager[5608]: Dump collected, initiating packaging
    Feb 19 06:24:56 phosphor-dump-manager[5612]: Dump packaging failed
    Updated status as xyz.openbmc_project.Common.Progress.OperationStatus.Failed
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: I7e55f61abde0128d6a5b8e9d1580eb3c5a6463b4

--- dump-extensions/openpower-dumps/op_dump_util.hpp
+++ dump-extensions/openpower-dumps/op_dump_util.hpp
@@ -8,20 +8,6 @@ namespace dump
 namespace util
 {
 
-/** @brief Package an OpenPOWER dump files from a location
- *  into the dump format with dump header.
- *  @param[in] dumpId - Id of the dump
- *  @param[in] allowedSize - Size available for the dump
- *  @param[in] inputDir - Locations of input dump pieces
- *  @param[in] packageDir - Location where packaged dump should be stored
- *  @param[in] dumpPrefix - Dump filename prefix
- *  @param[in] event - sd_event_loop
- */
-void captureDump(uint32_t dumpId, size_t allowedSize,
-                 const std::string& inputDir, const std::string& packageDir,
-                 const std::string& dumpPrefix,
-                 const phosphor::dump::EventPtr& event);
-
 /** @brief Check whether OpenPOWER dumps are enabled
  *
  * A xyz.openbmc_project.Dump.Create.Error.Disabled will be thrown
