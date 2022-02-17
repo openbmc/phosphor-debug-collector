efc95ca3534820f291a1a5588fe2a5e1ac6809b8
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2022-02-17 02:41:59 -0600

    Move BMC stored dump offload to a separate process
    Dup offload is a long running process and when
    dumps are offloading no other DBus can be
    serviced for a long time. Making dump offload
    a separate process and the status of the offload
    will be updated at the end of the offload.
    
    If the dump is progress any new request will be
    rejected if the URI to offload dump is same.
    
    Tests:
    
    Offloading 100M Dump across a slow conection:
    bash-3.2$ curl -k -H "X-Auth-Token: $bmc_token" -X GEThttps://${bmc}/redfish/v1/Systems/system/LogServices/Dump/Entries/Hostboot_26/attachment > f1
      % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
                                     Dload  Upload   Total   Spent    Left  Speed
    100  100M  100  100M    0     0  1713k      0  0:00:59  0:00:59 --:--:-- 3603k
    
    Offloading same dump at same time
    Feb 17 11:29:13 ever27bmc phosphor-dump-manager[4449]: Another offload in progres
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: Id5087e3749bcee9b807d359db73b73a1a43394a8

--- bmcstored_dump_entry.cpp
+++ bmcstored_dump_entry.cpp
@@ -1,9 +1,12 @@
 #include "bmc_dump_entry.hpp"
-#include "dump_manager.hpp"
+#include "dump_manager_bmcstored.hpp"
 #include "dump_offload.hpp"
+#include "xyz/openbmc_project/Common/error.hpp"
 
 #include <fmt/core.h>
 
+#include <phosphor-logging/elog-errors.hpp>
+#include <phosphor-logging/elog.hpp>
 #include <phosphor-logging/log.hpp>
 
 namespace phosphor
@@ -13,6 +16,8 @@ namespace dump
 namespace bmc_stored
 {
 using namespace phosphor::logging;
+using NotAllowed = sdbusplus::xyz::openbmc_project::Common::Error::NotAllowed;
+using Reason = xyz::openbmc_project::Common::NotAllowed::REASON;
 
 void Entry::delete_()
 {
@@ -37,8 +42,61 @@ void Entry::delete_()
 
 void Entry::initiateOffload(std::string uri)
 {
-    phosphor::dump::offload::requestOffload(path(), id, uri);
-    offloaded(true);
+    if ((isOffloadInProgress()) && (uri == offloadUri()))
+    {
+        log<level::ERR>(fmt::format("Another offload is in progress with same "
+                                    "URI({}), cannot continue",
+                                    offloadUri())
+                            .c_str());
+        elog<NotAllowed>(
+            Reason("Another offload is in progress, please try later"));
+    }
+
+    setOffloadInProgress();
+
+    log<level::INFO>(
+        fmt::format("offload started id({}) uri({})", id, uri).c_str());
+
+    phosphor::dump::Entry::initiateOffload(uri);
+    pid_t pid = fork();
+    if (pid == 0)
+    {
+        execl("/usr/bin/phosphor-offload-handler", "phosphor-offload-handler",
+              "--id", std::to_string(id).c_str(), "--path", path().c_str(),
+              "--uri", uri.c_str(), nullptr);
+        log<level::ERR>(fmt::format("Dump offload failure: Error occured while "
+                                    "starting offload id({})",
+                                    id)
+                            .c_str());
+        std::exit(EXIT_FAILURE);
+    }
+
+    if (pid > 0)
+    {
+        auto rc = sd_event_add_child(
+            dynamic_cast<phosphor::dump::bmc_stored::Manager&>(parent)
+                .eventLoop.get(),
+            nullptr, pid, WEXITED | WSTOPPED, callback, this);
+        if (0 > rc)
+        {
+            // Failed to add to event loop
+            log<level::ERR>(fmt::format("Dump offload: Error occurred during "
+                                        "the sd_event_add_child call, rc({})",
+                                        rc)
+                                .c_str());
+            throw std::runtime_error("Dump offload: Error occurred during the "
+                                     "sd_event_add_child call");
+        }
+    }
+    else
+    {
+        auto error = errno;
+        log<level::ERR>(
+            fmt::format("Dump offload: Error occurred during fork, errno({})",
+                        error)
+                .c_str());
+        throw std::runtime_error("Dump offload: Error occurred during fork");
+    }
 }
 
 } // namespace bmc_stored
