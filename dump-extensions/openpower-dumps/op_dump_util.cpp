9f6427a6a8fdd79faf91f43517a22313290cdae2
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2022-03-08 05:48:45 -0600

    OpenPOWER: Mpreboot: Do not remove InProgress system dump entry
    When the system dump is initiated by user a system dump
    entry will be created with status as in progress. During
    a power off all the entries of dumps stored in the host
    memory needs to be removed except the entry created for
    the system dump which causes the memory preserving
    reboot.
    
    Tests:
    Manually initiate a system dump and make sure
    dump is completed with same dump path as
    returned during the create command.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: Ib75353b6d359b0af85cd2365da91dd5a673e92c6

--- dump-extensions/openpower-dumps/op_dump_util.cpp
+++ dump-extensions/openpower-dumps/op_dump_util.cpp
@@ -19,6 +19,8 @@ namespace dump
 namespace util
 {
 
+constexpr auto MP_REBOOT_FILE = "/run/openbmc/mpreboot@0";
+
 bool isOPDumpsEnabled(sdbusplus::bus::bus& bus)
 {
     auto isEnabled = true;
@@ -45,6 +47,22 @@ bool isOPDumpsEnabled(sdbusplus::bus::bus& bus)
         isEnabled = false;
     }
     return isEnabled;
+}
+
+bool isInMpReboot()
+{
+    auto inMpReboot = false;
+    try
+    {
+        inMpReboot = std::filesystem::exists(MP_REBOOT_FILE);
+    }
+    catch (const std::filesystem::filesystem_error& e)
+    {
+        lg2::error("Error in determing whether the file:{FILE} exists,"
+                   "error: {ERROR_MSG}",
+                   "FILE", MP_REBOOT_FILE, "ERROR_MSG", e.what());
+    }
+    return inMpReboot;
 }
 
 bool isSystemDumpInProgress()
