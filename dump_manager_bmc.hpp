a9ded73662ae81738011e1ef0c1d322560a90909
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2022-03-18 07:21:57 -0500

    Make the dump filename configurable
    The dump filename can be platform dependant, so defining
    the dump filename format and the locations of dump id and
    timestamp in the meson options, so that can be overridden
    from the recipe.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: Ib51f49d6c2518c23965f2dd0e018795ab46b7b64

--- dump_manager_bmc.hpp
+++ dump_manager_bmc.hpp
@@ -18,9 +18,6 @@ namespace dump
 namespace bmc
 {
 
-constexpr auto BMC_DUMP_FILENAME_REGEX =
-    "obmcdump_([0-9]+)_([0-9]+).([a-zA-Z0-9]+)";
-
 using CreateIface = sdbusplus::server::object_t<
     sdbusplus::xyz::openbmc_project::Dump::server::Create>;
 
