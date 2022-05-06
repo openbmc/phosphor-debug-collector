93281b250a19620f23725a05e78929cb7b931474
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2022-05-06 06:21:11 -0500

    OpenPOWER: Limit User-Requested System Dumps
    System dumps can be several gigabytes in size. To conserve space,
    only one dump is stored at a time. When a new dump is requested,
    the existing dump will be discarded while creating a new one.
    In the case of system-generated dumps, which are triggered by
    critical errors, it is permissible to initiate a new dump even
    if an existing dump is already present in the host memory
    This commit adds a restriction on initiating new user-requested system
    dumps while a system dump is in progress or if one is already stored
    in host memory.
    
    New user-requested system dumps will be allowed once the existing
    dump is deleted or offloaded.
    
    This change applies only to user-requested dumps and does not impact
    system generated dumps.
    
    Test:
    Initiate system dump when another dump in progress.
    Force a system generated system dump when one dump is present
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: I4598912b0761669859f84a43ab4c60f47664b1e6

--- dump-extensions/openpower-dumps/op_dump_util.hpp
+++ dump-extensions/openpower-dumps/op_dump_util.hpp
@@ -19,6 +19,17 @@ namespace util
  */
 bool isOPDumpsEnabled(sdbusplus::bus::bus& bus);
 
+/** @brief Check whether memory preserving reboot is in progress
+ *  @return true - memory preserving reboot in progress
+ *          false - no memory preserving reboot is in progress
+ */
+bool isInMpReboot();
+
+/** @brief Check whether a system is in progress or available to offload.
+ *  @return true - A dump is in progress or available to offload
+ *          false - No dump in progress
+ */
+bool isSystemDumpInProgress();
 } // namespace util
 } // namespace dump
 } // namespace openpower
