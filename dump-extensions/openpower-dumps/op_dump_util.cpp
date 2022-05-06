42278a0dc6bdb3292df99b42f4c7d456143d665e
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2022-05-06 06:21:11 -0500

    OpenPOWER: Prevent new user-requested system dump when already in progress
    System dumps can be several gigabytes in size. To conserve space,
    only one dump is stored at a time. When a new dump is requested,
    the existing dump will be discarded while creating a new one.
    In the case of system-generated dumps, which are triggered by
    critical errors, it is permissible to initiate a new dump even
    if an existing dump is already present in the host memory
    This commit adds a restriction on initiating new user-requested system
    dumps while a system dump is in progress or if one is already stored
    in host memory.
    
    New user-requested system dumps will be allowed once the existing
    dump is deleted or offloaded.
    
    This change applies only to user-requested dumps and does not impact
    system generated dumps.
    
    Test:
    Initiate system dump when another dump in progress.
    Force a system generated system dump when one dump is present
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: I4598912b0761669859f84a43ab4c60f47664b1e6

--- dump-extensions/openpower-dumps/op_dump_util.cpp
+++ dump-extensions/openpower-dumps/op_dump_util.cpp
@@ -1,5 +1,6 @@
 #include "op_dump_util.hpp"
 
+#include "dump_utils.hpp"
 #include "xyz/openbmc_project/Common/error.hpp"
 #include "xyz/openbmc_project/Dump/Create/error.hpp"
 
@@ -66,6 +67,67 @@ bool isInMpReboot()
             ERROR_MSG}", "FILE", MP_REBOOT_FILE, "ERROR_MSG", e.what());
     }
     return inMpReboot;
+}
+
+bool isSystemDumpInProgress()
+{
+    using BiosBaseTableItem = std::pair<
+        std::string,
+        std::tuple<std::string, bool, std::string, std::string, std::string,
+                   std::variant<int64_t, std::string>,
+                   std::variant<int64_t, std::string>,
+                   std::vector<std::tuple<
+                       std::string, std::variant<int64_t, std::string>>>>>;
+    using BiosBaseTable = std::vector<BiosBaseTableItem>;
+
+    try
+    {
+        std::string dumpInProgress{};
+        auto bus = sdbusplus::bus::new_default();
+
+        auto retVal =
+            phosphor::dump::readDBusProperty<std::variant<BiosBaseTable>>(
+                bus, "xyz.openbmc_project.BIOSConfigManager",
+                "/xyz/openbmc_project/bios_config/manager",
+                "xyz.openbmc_project.BIOSConfig.Manager", "BaseBIOSTable");
+        const auto baseBiosTable = std::get_if<BiosBaseTable>(&retVal);
+        if (baseBiosTable == nullptr)
+        {
+            lg2::error("Util failed to read BIOSconfig property BaseBIOSTable");
+            return false;
+        }
+        for (const auto& item : *baseBiosTable)
+        {
+            const auto attributeName = std::get<0>(item);
+            auto attrValue = std::get<5>(std::get<1>(item));
+            auto val = std::get_if<std::string>(&attrValue);
+            if (val != nullptr && attributeName == "pvm_sys_dump_active")
+            {
+                dumpInProgress = *val;
+                break;
+            }
+        }
+        if (dumpInProgress.empty())
+        {
+            lg2::error(
+                "Util failed to read dump pvm_sys_dump_active  property value");
+            return false;
+        }
+        if (dumpInProgress == "Enabled")
+        {
+            lg2::info("A system dump is already in progress");
+            return true;
+        }
+    }
+    catch (const std::exception& ex)
+    {
+        lg2::error("Failed to read pvm_sys_dump_active error:{ERROR_MSG}",
+                   "ERROR_MSG", ex.what());
+        return false;
+    }
+
+    lg2::info("Another system dump is not in progress");
+    return false;
 }
 
 } // namespace util
--- dump-extensions/openpower-dumps/op_dump_util.hpp
+++ dump-extensions/openpower-dumps/op_dump_util.hpp
@@ -25,6 +25,12 @@ bool isOPDumpsEnabled(sdbusplus::bus::bus& bus);
  */
 bool isInMpReboot();
 
+/** @brief Check whether a system is in progress or
+ *  available to offload.
+ *  @return true - A dump is in progress or available to offload
+ *          false - No dump in progress
+ */
+bool isSystemDumpInProgress();
 } // namespace util
 } // namespace dump
 } // namespace openpower
