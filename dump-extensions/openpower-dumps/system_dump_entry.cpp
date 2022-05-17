35ecc664b683da6eca04f3acc61a4430b88b98ec
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2022-05-17 01:53:19 -0500

    OpenPower: Return unvailable if any error in deleting host dump
    If there is a failure in sending dump delete request to host
    for resource and system dump, return unavailable error.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: Ia85ef46c55c5a2b54af2b8632f94c3318ef1d4d1

--- dump-extensions/openpower-dumps/system_dump_entry.cpp
+++ dump-extensions/openpower-dumps/system_dump_entry.cpp
@@ -52,16 +52,28 @@ void Entry::delete_()
                                  dumpId, srcDumpID)
                          .c_str());
 
-    // Remove Dump entry D-bus object
-    phosphor::dump::Entry::delete_();
-
     // Remove host system dump when host is up by using source dump id
     // which is present in system dump entry dbus object as a property.
     if ((phosphor::dump::isHostRunning()) && (srcDumpID != INVALID_SOURCE_ID))
     {
-        phosphor::dump::host::requestDelete(srcDumpID,
-                                            TRANSPORT_DUMP_TYPE_IDENTIFIER);
+        try
+        {
+            phosphor::dump::host::requestDelete(srcDumpID,
+                                                TRANSPORT_DUMP_TYPE_IDENTIFIER);
+        }
+        catch (const std::exception& e)
+        {
+            log<level::ERR>(fmt::format("Error deleteing dump from host id({}) "
+                                        "host id({}) error({})",
+                                        dumpId, srcDumpID, e.what())
+                                .c_str());
+            elog<sdbusplus::xyz::openbmc_project::Common::Error::Unavailable>();
+        }
     }
+
+    // Remove Dump entry D-bus object
+    phosphor::dump::Entry::delete_();
+
     log<level::INFO>(
         fmt::format("System dump entry with id({}) is deleted", dumpId)
             .c_str());
