c32d6ecab20e1189f939b2c5bfbf311f37fd51dc
Marri Devender Rao <devenrao@in.ibm.com>
2022-06-28 23:12:14 -0500

    sdevent - inode entry created during dump generation is not released
    For every dump generated an "anon_inode:[pidfd]" entry is created
    for the associated process and the same is not released. When the
    number of inode entries reach the maximum count dump request will
    fail and the dump service takes a reset with a core dump.
    
    When a dump is requested a child process is spawned to perform
    dump collection. Parent process waits on the child process completion
    using sd-event-add-child systemd call.
    sd-event-add-child adds an inode entry which needs to be released
    during the callback method called after child process exit using
    unref but the same is not done.
    
    Now switching to sdeventplus::source::Child wrapper class for
    sd-event-add-child which takes care of releasing the inode fd.
    
    Tested:
    Without the fix for the 3 dumps created noticed 3 anon_inode:[pidfd]
    are added but not cleared
    root@rain135bmc:/proc/2044/fd# ls -la
    dr-x------    2 root     root             0 Jun 27 08:38 .
    dr-xr-xr-x    8 root     root             0 Jun 27 08:38 ..
    lr-x------    1 root     root            64 Jun 27 08:39 0 -> /dev/null
    lrwx------    1 root     root            64 Jun 27 08:39 1 -> socket:[21586]
    lrwx------    1 root     root            64 Jun 27 08:39 10 ->anon_inode:[timerfd]
    lrwx------    1 root     root            64 Jun 27 08:47 11 ->anon_inode:[pidfd]
    lrwx------    1 root     root            64 Jun 27 08:47 12 ->anon_inode:[pidfd]
    lrwx------    1 root     root            64 Jun 27 08:47 13 ->anon_inode:[pidfd]
    
    After the fix noticed all the anon_inode:[pidfd] are cleared
    dr-x------    2 root     root             0 Jun 27 08:38 .
    dr-xr-xr-x    8 root     root             0 Jun 27 08:38 ..
    lr-x------    1 root     root            64 Jun 27 08:39 0 -> /dev/null
    lrwx------    1 root     root            64 Jun 27 08:39 1 -> socket:[21586]
    lrwx------    1 root     root            64 Jun 27 08:39 10 ->anon_inode:[timerfd]
    
    Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>
    Change-Id: I9a2fec0a2287bfe978b811671c1cfeb1356e59d2

--- dump_manager_bmc.hpp
+++ dump_manager_bmc.hpp
@@ -5,9 +5,11 @@
 #include "watch.hpp"
 #include "xyz/openbmc_project/Dump/Internal/Create/server.hpp"
 
+#include <sdeventplus/source/child.hpp>
 #include <xyz/openbmc_project/Dump/Create/server.hpp>
 
 #include <filesystem>
+#include <map>
 
 namespace phosphor
 {
@@ -31,7 +33,7 @@ using Type =
     sdbusplus::xyz::openbmc_project::Dump::Internal::server::Create::Type;
 
 using Watch = phosphor::dump::inotify::Watch;
-
+using ::sdeventplus::source::Child;
 // Type to dreport type  string map
 static const std::map<Type, std::string> TypeMap = {
     {Type::ApplicationCored, "core"},
@@ -111,20 +113,6 @@ class Manager :
      */
     uint32_t captureDump(Type type, const std::vector<std::string>& fullPaths);
 
-    /** @brief sd_event_add_child callback
-     *
-     *  @param[in] s - event source
-     *  @param[in] si - signal info
-     *  @param[in] userdata - pointer to Watch object
-     *
-     *  @returns 0 on success, -1 on fail
-     */
-    static int callback(sd_event_source*, const siginfo_t*, void*)
-    {
-        // No specific action required in
-        // the sd_event_add_child callback.
-        return 0;
-    }
     /** @brief Remove specified watch object pointer from the
      *        watch map and associated entry from the map.
      *        @param[in] path - unique identifier of the map
@@ -150,6 +138,9 @@ class Manager :
      *        [path:watch object]
      */
     std::map<std::filesystem::path, std::unique_ptr<Watch>> childWatchMap;
+
+    /** @brief map of SDEventPlus child pointer added to event loop */
+    std::map<pid_t, std::unique_ptr<Child>> childPtrMap;
 };
 
 } // namespace bmc
