e803d66e2e99404638f3f7f073794bc545d93efd
Claire Weinan <cweinan@google.com>
2022-07-26 10:50:34 -0700

    Add CPER Log and Crashdump support in FaultLog
    The fault log manager receives D-Bus signals to be notified when new
    CPER logs and Crashdumps are available in BMC. When it receives the
    notification, it creates a corresponding fault log dump entry that
    includes the type and ID of the new log.
    
    The earliest fault log entry often contains the most relevant and
    detailed information that can help root-cause a fault. Therefore support
    is included for specially saving the first N entries of type T, where we
    currently support 2 entry types for T (CPER logs and crashdumps), and
    where N is configurable in meson_options.txt for each type. The
    intention is ensure that the earliest fault logs are saved, even if
    allocated space runs out, while still being able to receiving new
    entries (middle entries are subject to deletion).
    
    Note: in this patchset the fault log manager doesn't yet receive D-Bus
    signals indicating the deletion of CPER logs and crashdumps from BMC by
    the daemon they originate from. This functionality will be added later.
    
    Tested:
    Verified that CPER Log and crashdump D-Bus signals are received, and
    corresponding fault log entries are created.
    
    Example command for initiating crashdump:
    ```
    busctl call com.intel.crashdump /com/intel/crashdump com.intel.crashdump.Stored GenerateStoredLog s IERR
    ```
    
    For CPER log testing, locally modified bios_bmc_smm_error_logger to
    be able to force CPER log creation and send D-Bus signal to fault log
    manager.
    
    Example commands for verifying dump entry creation and D-Bus property
    values:
    ```
    busctl tree xyz.openbmc_project.Dump.Manager
    busctl introspect xyz.openbmc_project.Dump.Manager /xyz/openbmc_project/dump/faultlog/entry/1
    ```
    
    Also tested log entry deletion.
    Example command:
    ```
    busctl call xyz.openbmc_project.Dump.Manager /xyz/openbmc_project/dump/faultlog xyz.openbmc_project.Collection.DeleteAll DeleteAll
    ```
    
    Also tested invalid and missing createDump() parameters.
    Example command:
    ```
    busctl call xyz.openbmc_project.Dump.Manager  /xyz/openbmc_project/dump/faultlog xyz.openbmc_project.Dump.Create CreateDump a{sv} 2 "Type" s "MyInvalidType" "PrimaryLogId" s "1"
    ```
    
    Signed-off-by: Claire Weinan <cweinan@google.com>
    Change-Id: Ied5718ddbadb5117973ef92e3874e36570615d85

--- dump_manager_faultlog.cpp
+++ dump_manager_faultlog.cpp
@@ -30,21 +30,34 @@ using namespace sdbusplus::xyz::openbmc_project::Common::File::Error;
 using ErrnoOpen = xyz::openbmc_project::Common::File::Open::ERRNO;
 using PathOpen = xyz::openbmc_project::Common::File::Open::PATH;
 
+using InterfaceVariant = typename sdbusplus::utility::dedup_variant_t<
+    bool, uint8_t, uint16_t, int16_t, uint32_t, int32_t, uint64_t, int64_t,
+    size_t, ssize_t, double, std::string, sdbusplus::message::object_path>;
+
+using ChangedPropertiesType =
+    std::vector<std::pair<std::string, InterfaceVariant>>;
+
+using ChangedInterfacesType =
+    std::vector<std::pair<std::string, ChangedPropertiesType>>;
+
 sdbusplus::message::object_path
     Manager::createDump(phosphor::dump::DumpCreateParams params)
 {
-    log<level::INFO>("In dump_manager_fault.cpp createDump");
+    FaultDataType entryType = FaultDataType::Crashdump;
+    std::string primaryLogIdStr;
 
-    // Currently we ignore the parameters.
-    // TODO phosphor-debug-collector/issues/22: Check parameter values and
-    // exit early if we don't receive the expected parameters
-    if (params.empty())
+    log<level::INFO>("In dump_manager_faultlog.cpp createDump");
+
+    getAndCheckCreateDumpParams(params, entryType, primaryLogIdStr);
+
+    // To stay within the limit of MAX_NUM_FAULT_LOG_ENTRIES we need to remove
+    // an entry from the fault log map to make room for creating a new entry
+    if (entries.size() == MAX_NUM_FAULT_LOG_ENTRIES)
     {
-        log<level::INFO>("No additional parameters received");
-    }
-    else
-    {
-        log<level::INFO>("Got additional parameters");
+        // Save the earliest fault log entry to a saved entries map (if
+        // it qualifies to be saved), and remove it from the main fault
+        // log entries map.
+        saveEarliestEntry();
     }
 
     // Get the originator id and type from params
@@ -58,6 +71,10 @@ sdbusplus::message::object_path
     auto id = lastEntryId + 1;
     auto idString = std::to_string(id);
     auto objPath = std::filesystem::path(baseEntryPath) / idString;
+
+    log<level::INFO>(
+        fmt::format("next entry id: {}, entries.size(): {}", id, entries.size())
+            .c_str());
 
     std::filesystem::path faultLogFilePath(std::string(FAULTLOG_DUMP_PATH) +
                                            idString);
@@ -92,18 +109,13 @@ sdbusplus::message::object_path
     {
         log<level::INFO>("dump_manager_faultlog.cpp: add faultlog entry");
 
-        uint64_t timestamp =
-            std::chrono::duration_cast<std::chrono::microseconds>(
-                std::chrono::system_clock::now().time_since_epoch())
-                .count();
-
-        entries.insert(
-            std::make_pair(id, std::make_unique<faultlog::Entry>(
-                                   bus, objPath.c_str(), id, timestamp,
-                                   std::filesystem::file_size(faultLogFilePath),
-                                   faultLogFilePath,
-                                   phosphor::dump::OperationStatus::Completed,
-                                   originatorId, originatorType, *this)));
+        entries.insert(std::make_pair(
+            id,
+            std::make_unique<faultlog::Entry>(
+                bus, objPath.c_str(), id, generateTimestamp(),
+                std::filesystem::file_size(faultLogFilePath), faultLogFilePath,
+                phosphor::dump::OperationStatus::Completed, originatorId,
+                originatorType, entryType, primaryLogIdStr, *this, &entries)));
     }
     catch (const std::invalid_argument& e)
     {
@@ -118,6 +130,357 @@ sdbusplus::message::object_path
 
     log<level::INFO>("End of dump_manager_faultlog.cpp createDump");
     return objPath.string();
+}
+
+void Manager::deleteAll()
+{
+    log<level::INFO>("In dump_manager_faultlog.hpp deleteAll");
+
+    phosphor::dump::Manager::deleteAll();
+
+    auto iter = savedCperLogEntries.begin();
+    while (iter != savedCperLogEntries.end())
+    {
+        auto& entry = iter->second;
+        entry->delete_();
+        ++iter;
+    }
+
+    iter = savedCrashdumpEntries.begin();
+    while (iter != savedCrashdumpEntries.end())
+    {
+        auto& entry = iter->second;
+        entry->delete_();
+        ++iter;
+    }
+}
+
+void Manager::registerFaultLogMatches()
+{
+    log<level::INFO>("dump_manager_faultlog registerFaultLogMatches");
+
+    registerCrashdumpMatch();
+    registerCperLogMatch();
+}
+
+void Manager::registerCrashdumpMatch()
+{
+#ifdef FAULTLOG_MATCH_INTEL_CRASHDUMP
+    crashdumpMatch = std::make_unique<sdbusplus::bus::match_t>(
+        bus,
+        sdbusplus::bus::match::rules::propertiesChangedNamespace(
+            INTEL_CRASHDUMP_OBJPATH_NAMESPACE, INTEL_CRASHDUMP_INTERFACE) +
+            sdbusplus::bus::match::rules::sender(INTEL_CRASHDUMP_BUSNAME),
+
+        [this](sdbusplus::message_t& msg) {
+            if (msg.is_method_error())
+            {
+                log<level::ERR>(
+                    "dump_manager_faultlog got crashdump added error!");
+                return;
+            }
+
+            log<level::INFO>("Got new crashdump notification!");
+
+            std::string interface;
+            ChangedPropertiesType changedProps;
+            msg.read(interface, changedProps);
+
+            for (const auto& [changedProp, newValue] : changedProps)
+            {
+                if (changedProp == "Log")
+                {
+                    const auto* val = std::get_if<std::string>(&newValue);
+                    if (val == nullptr)
+                    {
+                        log<level::ERR>("Couldn't get Log property");
+                        return;
+                    }
+
+                    log<level::INFO>(fmt::format("Log: {}", *val).c_str());
+
+                    createDumpFromCrashdumpSignal(msg);
+                }
+            }
+        });
+#endif
+}
+
+void Manager::registerCperLogMatch()
+{
+#ifdef FAULTLOG_MATCH_BIOS_BMC_SMM_ERROR_LOGGER
+    cperLogMatch = std::make_unique<sdbusplus::bus::match_t>(
+        bus,
+        sdbusplus::bus::match::rules::interfacesAdded() +
+            sdbusplus::bus::match::rules::path_namespace(
+                BIOS_BMC_SMM_ERROR_LOGGER_OBJPATH_NAMESPACE) +
+            sdbusplus::bus::match::rules::sender(
+                BIOS_BMC_SMM_ERROR_LOGGER_BUSNAME),
+
+        [this](sdbusplus::message_t& msg) {
+            if (msg.is_method_error())
+            {
+                log<level::INFO>(
+                    "dump_manager_faultlog got cperLogMatch error!");
+            }
+
+            log<level::INFO>("Got new CPER Log notification!");
+
+            sdbusplus::message::object_path newObjPath;
+
+            ChangedPropertiesType changedProps;
+            ChangedInterfacesType changedInterfaces;
+            msg.read(newObjPath, changedInterfaces);
+
+            log<level::INFO>(
+                fmt::format("newObjPath: {}", newObjPath.str).c_str());
+
+            for (const auto& [changedInterface, changedProps] :
+                 changedInterfaces)
+            {
+                if (changedInterface == "xyz.openbmc_project.Common.FilePath")
+                {
+                    log<level::INFO>("changedInterface is "
+                                     "xyz.openbmc_project.Common.FilePath");
+
+                    for (const auto& [changedProp, newValue] : changedProps)
+                    {
+                        if (changedProp == "Path")
+                        {
+                            const auto* val =
+                                std::get_if<std::string>(&newValue);
+
+                            if (val == nullptr)
+                            {
+                                log<level::ERR>("Couldn't get Path property");
+                                return;
+                            }
+
+                            log<level::INFO>(
+                                fmt::format("Path: {}", *val).c_str());
+
+                            std::string cperLogPath(CPER_LOG_PATH);
+                            bool badPath = false;
+
+                            // Check path length
+                            if ((*val).size() <
+                                cperLogPath.size() + CPER_LOG_ID_STRING_LEN)
+                            {
+                                badPath = true;
+                                log<level::ERR>(
+                                    fmt::format("CPER_LOG_ID_STRING_LEN: {}",
+                                                CPER_LOG_ID_STRING_LEN)
+                                        .c_str());
+                            }
+                            // Check path prefix
+                            else if ((*val).compare(0, cperLogPath.size(),
+                                                    cperLogPath) != 0)
+                            {
+                                badPath = true;
+                            }
+
+                            if (badPath)
+                            {
+                                log<level::ERR>(
+                                    fmt::format("Unexpected CPER log path: {}",
+                                                *val)
+                                        .c_str());
+                            }
+                            else
+                            {
+                                std::string cperId = val->substr(
+                                    cperLogPath.size(), CPER_LOG_ID_STRING_LEN);
+                                std::map<std::string,
+                                         std::variant<std::string, uint64_t>>
+                                    cperLogMap;
+                                cperLogMap.insert(
+                                    std::pair<std::string, std::string>(
+                                        "Type", "CPER"));
+                                cperLogMap.insert(
+                                    std::pair<std::string, std::string>(
+                                        "PrimaryLogId", cperId));
+                                createDump(cperLogMap);
+                            }
+                        }
+                    }
+                }
+            }
+        });
+#endif
+}
+
+void Manager::createDumpFromCrashdumpSignal(sdbusplus::message_t& msg)
+{
+    std::string objpath = msg.get_path();
+
+    std::map<std::string, std::variant<std::string, uint64_t>> crashdumpMap;
+
+    crashdumpMap.insert(
+        std::pair<std::string, std::string>("Type", "Crashdump"));
+
+    crashdumpMap.insert(std::pair<std::string, std::string>(
+        "PrimaryLogId", std::filesystem::path(objpath).filename()));
+
+    createDump(crashdumpMap);
+}
+
+void Manager::getAndCheckCreateDumpParams(
+    const phosphor::dump::DumpCreateParams& params, FaultDataType& entryType,
+    std::string& primaryLogIdStr)
+{
+    using InvalidArgument =
+        sdbusplus::xyz::openbmc_project::Common::Error::InvalidArgument;
+    using Argument = xyz::openbmc_project::Common::InvalidArgument;
+    std::string value;
+
+    auto iter = params.find("Type");
+    if (iter == params.end())
+    {
+        log<level::ERR>("Required argument Type is missing");
+        elog<InvalidArgument>(Argument::ARGUMENT_NAME("TYPE"),
+                              Argument::ARGUMENT_VALUE("MISSING"));
+    }
+    else
+    {
+        try
+        {
+            value = std::get<std::string>(iter->second);
+        }
+        catch (const std::bad_variant_access& e)
+        {
+            // Exception will be raised if the input is not string
+            log<level::ERR>(
+                fmt::format("An invalid Type string is passed errormsg({})",
+                            e.what())
+                    .c_str());
+            elog<InvalidArgument>(Argument::ARGUMENT_NAME("TYPE"),
+                                  Argument::ARGUMENT_VALUE("INVALID INPUT"));
+        }
+
+        if (value == "Crashdump")
+        {
+            entryType = FaultDataType::Crashdump;
+        }
+        else if (value == "CPER")
+        {
+            entryType = FaultDataType::CPER;
+        }
+        else
+        {
+            log<level::ERR>("Unexpected entry type, not handled");
+            elog<InvalidArgument>(Argument::ARGUMENT_NAME("TYPE"),
+                                  Argument::ARGUMENT_VALUE("UNEXPECTED TYPE"));
+        }
+    }
+
+    iter = params.find("PrimaryLogId");
+    if (iter == params.end())
+    {
+        log<level::ERR>("Required argument PrimaryLogId is missing");
+        elog<InvalidArgument>(Argument::ARGUMENT_NAME("PRIMARYLOGID"),
+                              Argument::ARGUMENT_VALUE("MISSING"));
+    }
+    else
+    {
+        try
+        {
+            value = std::get<std::string>(iter->second);
+        }
+        catch (const std::bad_variant_access& e)
+        {
+            // Exception will be raised if the input is not string
+            log<level::ERR>(
+                fmt::format(
+                    "An invalid PrimaryLogId string is passed errormsg({})",
+                    e.what())
+                    .c_str());
+            elog<InvalidArgument>(Argument::ARGUMENT_NAME("PRIMARYLOGID"),
+                                  Argument::ARGUMENT_VALUE("INVALID INPUT"));
+        }
+
+        if (value.empty())
+        {
+            log<level::ERR>("Got empty PrimaryLogId string");
+            elog<InvalidArgument>(Argument::ARGUMENT_NAME("PRIMARYLOGID"),
+                                  Argument::ARGUMENT_VALUE("EMPTY STRING"));
+        }
+
+        primaryLogIdStr = value;
+    }
+}
+
+uint64_t Manager::generateTimestamp()
+{
+    uint64_t timestamp =
+        std::chrono::duration_cast<std::chrono::microseconds>(
+            std::chrono::system_clock::now().time_since_epoch())
+            .count();
+
+    // Ensure unique and increasing timestamps. In case the current
+    // generated timestamp is smaller or equal to the last generated
+    // timestamp, make the current timestamp the previous timestamp
+    // incremented by 1 microsecond.
+    if (timestamp <= prevTimestamp)
+    {
+        timestamp = prevTimestamp + 1;
+    }
+
+    prevTimestamp = timestamp;
+    return timestamp;
+}
+
+void Manager::saveEarliestEntry()
+{
+    auto earliestEntry = entries.begin();
+    uint32_t earliestEntryId = earliestEntry->first;
+    auto earliestEntryPtr = (earliestEntry->second).get();
+    FaultDataType earliestEntryType =
+        dynamic_cast<faultlog::Entry*>(earliestEntryPtr)->type();
+
+    size_t maxNumSavedEntries = 0;
+    std::map<uint32_t, std::unique_ptr<phosphor::dump::Entry>>* savedEntries;
+
+    switch (earliestEntryType)
+    {
+        case FaultDataType::CPER:
+            maxNumSavedEntries = MAX_NUM_SAVED_CPER_LOG_ENTRIES;
+            savedEntries = &savedCperLogEntries;
+            break;
+        case FaultDataType::Crashdump:
+            maxNumSavedEntries = MAX_NUM_SAVED_CRASHDUMP_ENTRIES;
+            savedEntries = &savedCrashdumpEntries;
+            break;
+        default:
+            earliestEntryPtr->delete_();
+            return;
+    }
+
+    log<level::INFO>(
+        fmt::format("dump_manager_faultlog.cpp: in saveEarliestEntry(). "
+                    "entry id: {}, type: {}, savedEntries->size(): {}",
+                    earliestEntryId, static_cast<uint32_t>(earliestEntryType),
+                    savedEntries->size())
+            .c_str());
+
+    // Check whether saved entries map has space for a new entry
+    if (savedEntries->size() < maxNumSavedEntries)
+    {
+        // Update earliest entry's parent map pointer to its saved entries map
+        dynamic_cast<phosphor::dump::faultlog::Entry*>(earliestEntryPtr)
+            ->parentMap = savedEntries;
+
+        // Insert earliest entry into saved entries map
+        savedEntries->insert(
+            std::make_pair(earliestEntryId, std::move(earliestEntry->second)));
+
+        // Erase earliest entry from fault log entries map
+        entries.erase(earliestEntryId);
+    }
+    else
+    {
+        // Delete earliest entry
+        earliestEntryPtr->delete_();
+    }
 }
 
 } // namespace faultlog
