9e1be8e309b9f696417fd7503a3312e90f51879f
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2023-05-31 14:58:47 -0500

    Move generated error map to cpp file
    This commit changes the generation location of error map
    from a header file (.hpp) to a source file (.cpp).
    This change enhances code organization, separates
    declaration from implementation.
    
    Change-Id: I01e4503e24ebf9045793014060107cca4ff440ad
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>

--- errors_map.mako.cpp
+++ errors_map.mako.cpp
@@ -1,11 +1,7 @@
 ## This file is a template.  The comment below is emitted
 ## into the rendered file; feel free to edit this file.
 // !!! WARNING: This is a GENERATED Code..Please do NOT Edit !!!
-#include <map>
-using EType = std::string;
-using Error = std::string;
-using ErrorList = std::vector<Error>;
-using ErrorMap = std::map<EType, std::vector<Error>>;
+#include "errors_map.hpp"
 
 const ErrorMap errorMap = {
 % for key, errors in errDict.items():
