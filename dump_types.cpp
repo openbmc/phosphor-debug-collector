fe27f5d1ef98b0ad832c1665c2f1dd2fb6f4ab45
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2023-06-29 05:35:07 -0500

    Add Ramoops dump to common create
    This commit adjusts the handling of ramoops (RAM OOPS)
    dumps to use the common BMC dump creation interface.
    Previously, when a ramoops dump was detected, the
    ramoops manager would inform the dump manager via an
    internal D-Bus interface. With the changes in this
    commit, upon detecting a ramoops event, the ramoops
    manager will now request the dump manager to create
    a BMC dump, incorporating the relevant ramoops data,
    via the common create DBus interface.
    
    Change-Id: I0437cfa6c63fe261ca9b51f6a90c8183abacd5c7
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>

--- dump_types.cpp
+++ dump_types.cpp
@@ -7,6 +7,8 @@ DUMP_TYPE_TABLE dumpTypeTable = {
     {"xyz.openbmc_project.Dump.Create.DumpType.UserRequested",
      {"user", "BMC_DUMP"}},
     {"xyz.openbmc_project.Dump.Create.DumpType.ApplicationCored",
-     {"core", "BMC_DUMP"}}};
+     {"core", "BMC_DUMP"}},
+    {"xyz.openbmc_project.Dump.Create.DumpType.Ramoops",
+     {"ramoops", "BMC_DUMP"}}};
 } // namespace dump
 } // namespace phosphor
