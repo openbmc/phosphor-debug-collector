d8ae4f722b0bdebe4d24b0cd40e70acd9a90c6dc
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2023-06-29 05:57:47 -0500

    Add elog dump type to common create
    This commit modifies the Elog BMC dump creation process
    to utilize the common BMC dump creation interface. Elog
    BMC dumps are a special type of dumps triggered upon the
    logging of certain predefined set of error logs. These
    dumps incorporate data based on the type of the error log.
    If no error log type is specified, a default  elog dump
    will be generated. In the existing process, upon occurrence
    of a predefined error log, the error log manager would
    inform the dump manager via an internal DBus interface. Now,
    with this change, the error log manager will request the
    dump manager to create an Elog BMC dump that includes the
    relevant error data, error type, and the object path of the
    error log entry via the common create DBus interface.
    
    Test:
    Create an InternalFailure and sure dump is created
    >busctl call xyz.openbmc_project.Logging \
     /xyz/openbmc_project/logging \
     xyz.openbmc_project.Logging.Create Create ssa{ss} \
     xyz.openbmc_project.Common.Error.InternalFailure \
     xyz.openbmc_project.Logging.Entry.Level.Error 0
    
    Trace:
    phosphor-dump-manager[542]: Initiating new BMC dump \
    with type: elog path: /xyz/openbmc_project/logging/entry/12
    
    phosphor-dump-manager[2918]: Report is available in \
    /var/lib/phosphor-debug-collector/dumps/6
    
    Change-Id: I734b052fc24e7893a61755790be49e8a1e594be5
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>

--- dump_types.cpp
+++ dump_types.cpp
@@ -9,12 +9,15 @@ DUMP_TYPE_TABLE dumpTypeTable = {
     {"xyz.openbmc_project.Dump.Create.DumpType.ApplicationCored",
      {DumpTypes::CORE, "BMC_DUMP"}},
     {"xyz.openbmc_project.Dump.Create.DumpType.Ramoops",
-     {DumpTypes::RAMOOPS, "BMC_DUMP"}}};
+     {DumpTypes::RAMOOPS, "BMC_DUMP"}},
+    {"xyz.openbmc_project.Dump.Create.DumpType.ErrorLog",
+     {DumpTypes::ELOG, "elog"}}};
 
 DUMP_TYPE_TO_STRING_MAP dumpTypeToStringMap = {
     {DumpTypes::USER, "user"},
     {DumpTypes::CORE, "core"},
     {DumpTypes::RAMOOPS, "ramoops"},
+    {DumpTypes::ELOG, "elog"},
 };
 } // namespace dump
 } // namespace phosphor
