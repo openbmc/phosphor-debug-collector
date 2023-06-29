163c497c42f0e89a02ed150c70b717d3d3fcecda
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2023-06-29 03:46:25 -0500

    BMCDump: dump subtype input parameter support
    ThBMCDump: dump subtype input parameter supportnot accepting
    any parameters to specify the type of dump or level of
    data to be collected. It was always creating a default
    user-requested dump.
    
    This commit allows for the specification of the dump type
    during the dump creation process. A new table has been
    added in a new header file which stores the valid dump
    types. When a new dump is requested, the specified dump
    type is verified against this table. If no dump type is
    mentioned, a user-requested BMC dump is created for
    backward compatibility. If an invalid dump type is
    mentioned, an Invalid Argument exception is thrown.
    
    Change-Id: I79f68be6ac6892ac7754b7221db64c22330b1822
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>

---
+++ dump_types.cpp
@@ -0,0 +1,10 @@
+#include "dump_types.hpp"
+namespace phosphor
+{
+namespace dump
+{
+DUMP_TYPE_TABLE dumpTypeTable = {
+    {"xyz.openbmc_project.Dump.Create.DumpType.UserRequested",
+     {"user", "BMCi_DUMP"}}};
+} // namespace dump
+} // namespace phosphor
---
+++ dump_types.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+namespace phosphor
+{
+namespace dump
+{
+// Overall dump category for example BMC dump
+using DUMP_CATEGORY = std::string;
+
+// Dump type
+using DUMP_TYPE = std::string;
+
+// Dump collection indicator
+using DUMP_COLLECTION_TYPE = std::string;
+
+// A table of dump types
+using DUMP_TYPE_TABLE =
+    std::unordered_map<DUMP_TYPE,
+                       std::pair<DUMP_COLLECTION_TYPE, DUMP_CATEGORY>>;
+
+extern DUMP_TYPE_TABLE dumpTypeTable;
+} // namespace dump
+} // namespace phosphor
