8bea993c3df316690e57ece4ccd07bdd3707574b
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2023-06-29 05:17:35 -0500

    Add support core dump in common create
    This commit modifies the interaction between the systemd
    core dump monitor and the BMC dump manager. Earlier, when
    a process managed by systemd crashed, it generated a core
    dump. The core dump monitor would then inform the dump
    manager about this event through a dedicated internal
    D-Bus interface.
    
    With this commit, we change this approach and leverage
    the common BMC dump creation interface. Now, when a core
    dump is generated, the core dump monitor requests the dump
    manager to create a specific BMC dump that incorporates
    this core dump data.
    
    Tests:
    Create a core dump and make sure BMC dump with core
    is created
    
    >kill -3 2232
    Trace:
    phosphor-dump-manager[542]: Initiating new BMC dump with \
    type: core path: /var/lib/systemd/coredump/core.openpower\
    -occ-c.0.0b16f513a5bc43f98ea11ed525f1a0c5.2232.16889058660\
    00000.zst
    
    phosphor-dump-manager[2738]: Report is available in /var/\
    lib/phosphor-debug-collector/dumps/5
    
    Change-Id: Ic2cc723babccb0f8d36504992996bcf418db8463
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>

--- dump_types.cpp
+++ dump_types.cpp
@@ -5,10 +5,13 @@ namespace dump
 {
 DUMP_TYPE_TABLE dumpTypeTable = {
     {"xyz.openbmc_project.Dump.Create.DumpType.UserRequested",
-     {DumpTypes::USER, "BMC_DUMP"}}};
+     {DumpTypes::USER, "BMC_DUMP"}},
+    {"xyz.openbmc_project.Dump.Create.DumpType.ApplicationCored",
+     {DumpTypes::CORE, "BMC_DUMP"}}};
 
 DUMP_TYPE_TO_STRING_MAP dumpTypeToStringMap = {
     {DumpTypes::USER, "user"},
+    {DumpTypes::CORE, "core"}
 };
 } // namespace dump
 } // namespace phosphor
