5298d5624d3910fa653e688dd5d9def660a7fe83
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2023-06-29 03:46:25 -0500

    BMCDump: dump subtype input parameter support
    The BMC dump is not accepting any parameters to specify the type of
    dump or level of data to be collected. It was always creating a default
    user-requested dump.
    
    This commit allows for the specification of the dump type during the
    dump creation process. A new table has been added in a new header file
    which stores the valid dump types. When a new dump is requested, the
    specified dump type is verified against this table. If no dump type is
    mentioned, a user-requested BMC dump is created for backward
    compatibility. If an invalid dump type is mentioned, an Invalid
    Argument exception is thrown.
    
    Tested:
    - Create BMC dump with no arguments
    - Create BMC dump with type as user
    - Create a error log dump and make sure other types of dumps
      which is using internal interface is not impacted.
    
    Change-Id: I79f68be6ac6892ac7754b7221db64c22330b1822
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>

---
+++ dump_types.cpp
@@ -0,0 +1,39 @@
+#include "dump_types.hpp"
+namespace phosphor
+{
+namespace dump
+{
+DUMP_TYPE_TABLE dumpTypeTable = {
+    {"xyz.openbmc_project.Dump.Create.DumpType.UserRequested",
+     {DumpTypes::USER, "BMC_DUMP"}}};
+
+DUMP_TYPE_TO_STRING_MAP dumpTypeToStringMap = {
+    {DumpTypes::USER, "user"},
+};
+
+std::optional<std::string> dumpTypeToString(const DumpTypes& dt)
+{
+    auto it = dumpTypeToStringMap.find(dt);
+    if (it != dumpTypeToStringMap.end())
+    {
+        return it->second;
+    }
+    return std::nullopt;
+}
+
+std::optional<DumpTypes> stringToDumpType(const std::string& str)
+{
+    auto it = std::ranges::find_if(dumpTypeToStringMap,
+                                   [&str](const auto& pair) {
+        return pair.second == str;
+    });
+
+    if (it != dumpTypeToStringMap.end())
+    {
+        return it->first;
+    }
+    return std::nullopt;
+}
+
+} // namespace dump
+} // namespace phosphor
