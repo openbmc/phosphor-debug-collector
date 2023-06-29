b4ae4383ea23e1bc04845ad939bfd8f0d4b8a5dd
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2023-06-29 03:46:25 -0500

    BMCDump: dump subtype input parameter support
    The BMC dump is not accepting any parameters to specify the type of
    dump or level of data to be collected. It was always creating a default
    user-requested dump.
    
    This commit allows for the specification of the dump type during the
    dump creation process. A new table has been added in a new header file
    which stores the valid dump types. When a new dump is requested, the
    specified dump type is verified against this table. If no dump type is
    mentioned, a user-requested BMC dump is created for backward
    compatibility. If an invalid dump type is mentioned, an Invalid
    Argument exception is thrown.
    
    Tested:
    - Create BMC dump with no arguments
    - Create BMC dump with type as user
    - Create a error log dump and make sure other types of dumps
      which is using internal interface is not impacted.
    
    Change-Id: I79f68be6ac6892ac7754b7221db64c22330b1822
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>

---
+++ dump_types.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <optional>
+#include <ranges>
+#include <string>
+#include <unordered_map>
+
+namespace phosphor
+{
+namespace dump
+{
+// Overall dump category for example BMC dump
+using DUMP_CATEGORY = std::string;
+
+// Dump type
+using DUMP_TYPE = std::string;
+
+// Dump collection indicator
+using DUMP_COLLECTION_TYPE = std::string;
+
+// Dump types
+enum class DumpTypes
+{
+    USER,
+};
+
+// A table of dump types
+using DUMP_TYPE_TABLE =
+    std::unordered_map<DUMP_TYPE, std::pair<DumpTypes, DUMP_CATEGORY>>;
+
+// Mapping between dump type and dump collection type string
+using DUMP_TYPE_TO_STRING_MAP =
+    std::unordered_map<DumpTypes, DUMP_COLLECTION_TYPE>;
+
+extern DUMP_TYPE_TABLE dumpTypeTable;
+extern DUMP_TYPE_TO_STRING_MAP dumpTypeToStringMap;
+
+/**
+ * @brief Converts a DumpTypes enum value to dump name.
+ *
+ * @param[in] dt The DumpTypes value to be converted.
+ * @return Name of the dump as strung, std::nullopt if not found.
+ */
+std::optional<std::string> dumpTypeToString(const DumpTypes& dt);
+
+/**
+ * @brief Converts dump name to its corresponding DumpTypes enum value.
+ *
+ * @param[in] str The string to be converted to a DumpTypes value.
+ * @return The DumpTypes value that corresponds to the name or std::nullopt if
+ * not found.
+ */
+std::optional<DumpTypes> stringToDumpType(const std::string& str);
+
+} // namespace dump
+} // namespace phosphor
