c29b4d90d8895330f249ae7be4101a0ef53bcbe1
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2023-07-08 09:28:10 -0500

    Use notifyDump instead of notify
    Change-Id: I16b228e1de81cbede127133f3b786ddf99f4c968
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>

--- dump-extensions/openpower-dumps/dump_manager_system.hpp
+++ dump-extensions/openpower-dumps/dump_manager_system.hpp
@@ -1,10 +1,11 @@
 #pragma once
 
+#include "com/ibm/Dump/Notify/server.hpp"
 #include "dump_manager.hpp"
 #include "dump_utils.hpp"
 #include "host_transport_exts.hpp"
 #include "new_dump_entry.hpp"
-#include "xyz/openbmc_project/Dump/NewDump/server.hpp"
+#include "op_dump_util.hpp"
 
 #include <sdbusplus/bus.hpp>
 #include <sdbusplus/server/object.hpp>
@@ -17,8 +18,9 @@ namespace system
 {
 
 constexpr uint32_t INVALID_SOURCE_ID = 0xFFFFFFFF;
-using NotifyIface = sdbusplus::server::object_t<
-    sdbusplus::xyz::openbmc_project::Dump::server::NewDump>;
+using NotifyIface =
+    sdbusplus::server::object_t<sdbusplus::com::ibm::Dump::server::Notify>;
+using NotifyDumpType = sdbusplus::common::com::ibm::dump::Notify::DumpType;
 
 /** @class Manager
  *  @brief System Dump  manager implementation.
@@ -61,7 +63,8 @@ class Manager :
      *  @param[in] dumpId - Id from the source of the dump.
      *  @param[in] size - Size of the dump.
      */
-    void notify(uint32_t dumpId, uint64_t size) override;
+    void notifyDump(uint32_t dumpId, uint64_t size, NotifyDumpType type,
+                    uint32_t token = 0) override;
 
     /** @brief Implementation for CreateDump
      *  Method to create a new system dump entry when user
@@ -75,8 +78,8 @@ class Manager :
   private:
     phosphor::dump::host::HostTransport& hostTransport;
 
-    phosphor::dump::BaseEntry* getInProgressEntry(uint32_t dumpId,
-                                                  uint64_t size);
+    phosphor::dump::BaseEntry*
+        getInProgressEntry(uint32_t dumpId, uint64_t size, uint32_t token = 0);
     bool isHostStateValid();
     std::string createEntry(uint32_t dumpId, uint64_t size,
                             phosphor::dump::OperationStatus status,
