361cba710c99db8c64cba31519295acffec5bb83
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2023-07-10 13:58:24 -0500

    Refactor: Add new dump entry class
    This commit introduces a templated dump entry class, named Entry, under
    a new namespace to avoid collision with the existing dump entry class.
    The new Entry class is derived from the recently introduced base entry
    class and is intended to be instantiated with two specific template
    parameters.
    
    The first parameter is the interface for dump entry, and the second is
    a helper class that implements specific variations for each dump type.
    
    The Entry class provides a concrete implementation for the
    xyz.openbmc_project.Dump.Entry DBus API. By incorporating the interface
    type and dump-specific functionality as template parameters, this class
    offers a robust and flexible framework for handling different dump types.
    
    Tested:
    - BMC dump create, list and delete
    
    Build with p10bmc and master
    
    Change-Id: I9f6016b8724731fd6ea357fda54b047da53a6395
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>

---
+++ new_dump_entry.hpp
@@ -0,0 +1,145 @@
+#pragma once
+
+#include "base_dump_entry.hpp"
+
+#include <sdbusplus/bus.hpp>
+
+#include <cstring>
+#include <filesystem>
+
+namespace phosphor
+{
+namespace dump
+{
+class Manager;
+namespace new_
+{
+
+template <typename T>
+using DumpEntryIface = sdbusplus::server::object_t<T>;
+
+using OperationStatus =
+    sdbusplus::xyz::openbmc_project::Common::server::Progress::OperationStatus;
+
+using OriginatorTypes = sdbusplus::xyz::openbmc_project::Common::server::
+    OriginatedBy::OriginatorTypes;
+
+/** @class Entry
+ *  @brief Concrete derived class for Dump Entry.
+ *  @details This class provides a templated concrete implementation for the
+ *  xyz.openbmc_project.Dump.Entry DBus API, derived from the BaseEntry class.
+ *
+ *  The Entry class is intended to be instantiated with a specific
+ *  DBus interface type as the template parameter T, providing functionality
+ *  specific to the corresponding dump type.
+ *
+ */
+template <typename T, typename U>
+class Entry : public BaseEntry, virtual public DumpEntryIface<T>
+{
+    friend U;
+
+  public:
+    Entry() = delete;
+    Entry(const Entry&) = delete;
+    Entry& operator=(const Entry&) = delete;
+    Entry(Entry&&) = delete;
+    Entry& operator=(Entry&&) = delete;
+    ~Entry() = default;
+
+    /** @brief Constructor for the Dump Entry Object
+     *  @param[in] bus - Bus to attach to.
+     *  @param[in] objPath - Object path to attach to
+     *  @param[in] dumpId - Dump id.
+     *  @param[in] timeStamp - Dump creation timestamp
+     *             since the epoch.
+     *  @param[in] dumpSize - Dump file size in bytes.
+     *  @param[in] file - Name of dump file.
+     *  @param[in] originId - Id of the originator of the dump
+     *  @param[in] originType - Originator type
+     *  @param[in] parent - The dump entry's parent.
+     */
+    Entry(sdbusplus::bus_t& bus, const std::string& objPath, uint32_t dumpId,
+          uint64_t timeStamp, uint64_t dumpSize,
+          const std::filesystem::path& file, OperationStatus dumpStatus,
+          std::string originId, OriginatorTypes originType,
+          phosphor::dump::Manager& parent) :
+        phosphor::dump::CommonIfaces(
+            bus, objPath.c_str(),
+            phosphor::dump::CommonIfaces::action::emit_no_signals),
+        DumpEntryIface<T>(bus, objPath.c_str(),
+                          DumpEntryIface<T>::action::emit_no_signals),
+        BaseEntry(bus, objPath, dumpId, timeStamp, dumpSize, file, dumpStatus,
+                  originId, originType, parent),
+        helper(*this, file)
+    {
+        this->phosphor::dump::new_::DumpEntryIface<T>::emit_object_added();
+    };
+
+    /** @brief Delete this d-bus object.
+     */
+    void delete_() override
+    {
+        helper.delete_();
+        BaseEntry::delete_();
+    }
+
+    /** @brief Method to initiate the offload of dump
+     *  @param[in] uri - URI to offload dump
+     */
+    void initiateOffload(std::string uri) override
+    {
+        BaseEntry::initiateOffload(uri);
+        helper.initiateOffload(id, uri);
+        offloaded(true);
+    }
+
+    /** @brief Method to get the file handle of the dump
+     *  @returns A Unix file descriptor to the dump file
+     *  @throws sdbusplus::xyz::openbmc_project::Common::File::Error::Open on
+     *  failure to open the file
+     *  @throws sdbusplus::xyz::openbmc_project::Common::Error::Unavailable if
+     *  the file string is empty
+     */
+    sdbusplus::message::unix_fd getFileHandle() override
+    {
+        return helper.getFileHandle(id);
+    }
+
+    /** @brief Method to update an existing dump entry, once the dump creation
+     *  is completed this function will be used to update the entry which got
+     *  created during the dump request.
+     *  @param[in] timeStamp - Dump creation timestamp
+     *  @param[in] fileSize - Dump file size in bytes.
+     *  @param[in] file - Name of dump file.
+     */
+    void markComplete(uint64_t timeStamp, uint64_t fileSize,
+                      const std::filesystem::path& filePath)
+    {
+        size(fileSize);
+        // TODO: Handled dump failed case with #ibm-openbmc/2808
+        status(OperationStatus::Completed);
+        helper.setFile(filePath);
+        // TODO: serialization of this property will be handled with
+        // #ibm-openbmc/2597
+        completedTime(timeStamp);
+    }
+
+    /** @brief Updates an existing dump entry once the dump creation is
+     * completed
+     *  @param[in] timeStamp - Dump creation timestamp
+     *  @param[in] dumpSize - Dump size in bytes.
+     *  @param[in] sourceId - DumpId provided by the source.
+     */
+    virtual void markCompleteWithSourceId(uint64_t /*timeStamp*/,
+                                          uint64_t /*fileSize*/,
+                                          uint32_t /*sourceId*/)
+    {}
+
+  private:
+    U helper;
+};
+
+} // namespace new_
+} // namespace dump
+} // namespace phosphor
