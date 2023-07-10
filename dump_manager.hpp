1429edb368eebd913cc8c0945c9e24536dc70043
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2023-07-10 12:58:44 -0500

    Refactor: Add BaseEntry class
    This commit introduces a new base entry class for all dump
    entries in the openpower-debug-collector. While a base dump
    entry class already exists, this change is aimed at
    reducing the level of inheritance and unifying common code
    across different dump entry types.
    
    The newly introduced base entry class houses all common
    methods and attributes. The existing dump entry class has
    been streamlined to prevent breaking the current code.
    This change is a preliminary step towards constructing
    a unified dump entry class composed of diverse functions
    for various dump types.
    
    A non-templated base class is also necessary for inclusion
    in the list of dump entries in the common base dump manager.
    This change provides a foundation for further optimizations
    and simplifications.
    
    Change-Id: Ib9f3f27a29da5a92cbe62d69668087e4e478acce
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>

--- dump_manager.hpp
+++ dump_manager.hpp
@@ -25,7 +25,7 @@ using Iface = sdbusplus::server::object_t<
  */
 class Manager : public Iface
 {
-    friend class Entry;
+    friend class BaseEntry;
 
   public:
     Manager() = delete;
@@ -69,7 +69,7 @@ class Manager : public Iface
     sdbusplus::bus_t& bus;
 
     /** @brief Dump Entry dbus objects map based on entry id */
-    std::map<uint32_t, std::unique_ptr<Entry>> entries;
+    std::map<uint32_t, std::unique_ptr<BaseEntry>> entries;
 
     /** @brief Id of the last Dump entry */
     uint32_t lastEntryId;
