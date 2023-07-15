2ba8ee48a8ce4a2ad1a595ff506656606e8fb499
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2023-07-15 12:16:41 -0500

    Make host transport to class structure
    This commit refactors the HostTransport related code by converting
    the associated functions into a Singleton class. The Singleton
    pattern is used to ensure that a class has only one instance
    and provides a global point of access to it. This improves
    modularity, code organization, and readability, while maintaining
    a unified access point for HostTransport instances.
    
    Key Changes:
    - Created HostTransport class within the phosphor::dump::host namespace.
    - Encapsulated related functions within this class.
    - Made the constructor private and added a public getInstance()
      method to ensure the Singleton pattern.
    - Singleton implementation for HostTransport instances by type.
    
    This refactor does not change any functionality but sets the foundation
    for future enhancements and makes the code easier to manage and understand.
    
    Change-Id: I7560140a374d2a04e6baf03ce0ee496abb73c418
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>

--- host-transport-extensions/default/default.cpp
+++ host-transport-extensions/default/default.cpp
@@ -1,3 +1,5 @@
+#include "host_transport_exts.hpp"
+
 #include <cstdint>
 #include <stdexcept>
 
@@ -7,12 +9,12 @@ namespace dump
 {
 namespace host
 {
-void requestOffload(uint32_t)
+void HostTransport::requestOffload(uint32_t)
 {
     throw std::runtime_error("Hostdump offload method not specified");
 }
 
-void requestDelete(uint32_t, uint32_t)
+void HostTransport::requestDelete(uint32_t)
 {
     throw std::runtime_error("Hostdump delete method not specified");
 }
--- host_transport_exts.hpp
+++ host_transport_exts.hpp
@@ -1,28 +1,51 @@
+#pragma once
+
 namespace phosphor
 {
 namespace dump
 {
 namespace host
 {
+class HostTransport
+{
+  public:
+    // Delete the copy constructor and copy assignment operator.
+    HostTransport(const HostTransport&) = delete;
+    HostTransport& operator=(const HostTransport&) = delete;
 
-/**
- * @brief Initiate offload of the dump with provided id
- *
- * @param[in] id - The Dump Source ID.
- *
- */
-void requestOffload(uint32_t id);
+    // Provide a static method to get the instance.
+    static HostTransport* getInstance(uint32_t dumpType)
+    {
+        static std::unordered_map<uint32_t, std::unique_ptr<HostTransport>>
+            instances;
 
-/**
- * @brief Request to delete dump
- *
- * @param[in] id - The Dump Source ID.
- * @param[in] type - transport defined type of the dump.
- * @return NULL
- *
- */
-void requestDelete(uint32_t id, uint32_t type);
+        // Create a new instance if it doesn't exist, or return existing one.
+        auto it = instances.find(dumpType);
+        if (it == instances.end())
+        {
+            // Create a new instance and insert it into the map.
+            std::unique_ptr<HostTransport> newInstance(
+                new HostTransport(dumpType));
+            auto result = instances.insert(
+                std::make_pair(dumpType, std::move(newInstance)));
 
+            return result.first->second.get();
+        }
+        else
+        {
+            return it->second.get();
+        }
+    }
+
+    void requestOffload(uint32_t id);
+    void requestDelete(uint32_t id);
+
+  private:
+    // Make the constructor private.
+    HostTransport(uint32_t type) : dumpType(type) {}
+
+    uint32_t dumpType;
+};
 } // namespace host
 } // namespace dump
 } // namespace phosphor
