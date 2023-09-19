4d86a3ebbf3191af46d8b5c8299779b19662a0fe
Deepa Karthikeyan <deepakala.karthikeyan@ibm.com>
2023-09-19 07:46:44 -0500

    Fix compiler warning
    Fix the following warning, as any warning will be treated as error
    ../../../dump_utils.hpp:38:9: warning: Value stored to 'event' is never read
    
    event = sd_event_unref(event);
    
    Change-Id: I020c2b2028e19ce8998754a38cb55830792a0118
    Signed-off-by: Deepa Karthikeyan <deepakala.karthikeyan@ibm.com>

--- dump_utils.hpp
+++ dump_utils.hpp
@@ -35,7 +35,7 @@ struct EventDeleter
 {
     void operator()(sd_event* event) const
     {
-        event = sd_event_unref(event);
+        sd_event_unref(event);
     }
 };
 using EventPtr = std::unique_ptr<sd_event, EventDeleter>;
