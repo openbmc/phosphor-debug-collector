18c7d92ecd83ea406f17a2203c12976467766f37
Patrick Williams <patrick@stwcx.xyz>
2024-02-26 10:36:25 -0600

    lg2: use direct enum conversion
    lg2 now allows logging an enum directly without extra casting.  Remove
    the `underlying_cast` to simplify the code.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: I3415be5f7e9be2302c870b5499f96cda3545d0fa

--- host-transport-extensions/pldm/oem/ibm/pldm_oem_cmds.cpp
+++ host-transport-extensions/pldm/oem/ibm/pldm_oem_cmds.cpp
@@ -176,10 +176,8 @@ void requestDelete(uint32_t dumpId, uint32_t dumpType)
             "Failed to encode pldm FileAck to delete host dump, "
             "SRC_DUMP_ID: {SRC_DUMP_ID}, PLDM_FILE_IO_TYPE: {PLDM_DUMP_TYPE}, "
             "PLDM_RETURN_CODE: {RET_CODE}",
-            "SRC_DUMP_ID", dumpId, "PLDM_DUMP_TYPE",
-            static_cast<std::underlying_type<pldm_fileio_file_type>::type>(
-                pldmDumpType),
-            "RET_CODE", retCode);
+            "SRC_DUMP_ID", dumpId, "PLDM_DUMP_TYPE", pldmDumpType "RET_CODE",
+            retCode);
         elog<NotAllowed>(Reason("Host dump deletion via pldm is not "
                                 "allowed due to encode fileack failed"));
     }
