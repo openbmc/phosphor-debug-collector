07e10c7d44c7647b6055e97f6df2cbc2e5b319b6
Patrick Williams <patrick@stwcx.xyz>
2024-02-26 10:36:25 -0600

    lg2: use direct enum conversion
    lg2 now allows logging an enum directly without extra casting.  Remove
    the `underlying_cast` to simplify the code.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: I3415be5f7e9be2302c870b5499f96cda3545d0fa

--- host-transport-extensions/pldm/common/pldm_utils.cpp
+++ host-transport-extensions/pldm/common/pldm_utils.cpp
@@ -25,10 +25,8 @@ int openPLDM()
     if (fd < 0)
     {
         auto e = errno;
-        lg2::error(
-            "pldm_open failed, errno: {ERRNO}, FD: FD", "ERRNO", e, "FD",
-            static_cast<std::underlying_type<pldm_requester_error_codes>::type>(
-                fd));
+        lg2::error("pldm_open failed, errno: {ERRNO}, FD: FD", "ERRNO", e, "FD",
+                   fd);
         elog<NotAllowed>(
             Reason("Required host dump action via pldm is not allowed due "
                    "to pldm_open failed"));
