b3e5c580ddba384372f224bfcf4d15aae1a54a16
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2024-05-06 03:09:31 -0500

    OpenPOWER: Transition to Unified Dump Manager
    This commit marks a shift in the management of dumps within the
    OpenPOWER dump management. It transitions the previously isolated
    openpower::dump::system::Manager to a more encompassing
    openpower::dump::Manager. This change consolidates the handling of
    various dump types under a single entity and removes the code
    responsible for initiating dumps from this manager.
    
    Detailed Changes:
    1. Manager Renaming and Repurposing: The system dump manager has been
    renamed and repurposed to serve as a universal manager for all types
    of OpenPOWER dumps.
    
    2. Removal of Dump Initiation: The initiation code for dumps, previously
    embedded within the dump manager, has been extracted and delegated to
    more appropriate components within the system.
    
    3. Matching Redfish Interface: The unified manager adheres to the
    Redfish way(Systems/system/LogService/dump) by providing a single
    interface for all OpenPOWER dumps.
    
    4. Identification Process: The identification of dumps is now solely
    based on their unique IDs
    
    5. Implementation of a New Notify Interface: A new Notify interface
    has been implemented, which is equipped to accept a dump type parameter.
    
    Rationale:
    - This consolidation minimizes redundancy, reduces complexity.
    - Defines the role of the phosphor-dump-manager strictly for external
    interface operations like listing, deleting, and offloading dumps for
    OpenPOWER dumps. This delineation ensures that the phosphor-dump-manager
    is not overloaded with dump collection responsibilities specific to
    OpenPOWER, reducing potential impacts on the phosphor-debug-collector
    from OpenPOWER-specific implementations.
    
    Change-Id: I8c413e7e7ed074dc642e6c64dae6fa9a6fe35010
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>

--- dump-extensions/openpower-dumps/dump-extensions.cpp
+++ dump-extensions/openpower-dumps/dump-extensions.cpp
@@ -4,8 +4,8 @@
 
 #include "dump-extensions/openpower-dumps/openpower_dumps_config.h"
 
+#include "dump_manager_openpower.hpp"
 #include "dump_manager_resource.hpp"
-#include "dump_manager_system.hpp"
 
 namespace phosphor
 {
@@ -14,8 +14,9 @@ namespace dump
 
 void loadExtensions(sdbusplus::bus_t& bus, DumpManagerList& dumpList)
 {
-    dumpList.push_back(std::make_unique<openpower::dump::system::Manager>(
-        bus, SYSTEM_DUMP_OBJPATH, SYSTEM_DUMP_OBJ_ENTRY));
+    dumpList.push_back(std::make_unique<openpower::dump::Manager>(
+        bus, openpower::dump::OP_DUMP_OBJ_PATH,
+        openpower::dump::OP_BASE_ENTRY_PATH));
     dumpList.push_back(std::make_unique<openpower::dump::resource::Manager>(
         bus, RESOURCE_DUMP_OBJPATH, RESOURCE_DUMP_OBJ_ENTRY));
 }
