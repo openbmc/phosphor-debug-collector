d5869cb9062484ae5dfdc8abfa6a4bec23ef1e27
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2024-05-06 03:09:31 -0500

    OpenPOWER: Transition to Unified Dump Manager
    This commit is part of a chain of commits consolidating all OpenPOWER
    dump managers into a single unified dump manager. The goal is to
    streamline dump management by centralizing all dump types under one
    manager and keeping this service focused solely on hosting dump entries.
    
    Detailed Changes:
    1. Unified System Dump Manager: The system dump manager has been
       repurposed as a universal manager for all OpenPOWER dumps,
       consolidating management under one namespace.
    
    2. Removal of Dump Initiation Code: The dump initiation code has
       been extracted from the dump manager and delegated to appropriate
       system components.
    
    3. Redfish Interface Compatibility: The unified manager adheres to
       the Redfish standard by providing a single interface for all
       OpenPOWER dumps at `Systems/system/LogService/dump`.
    
    4. Unique ID-based Identification: Dumps are now identified solely by
       their unique IDs, streamlining the identification process.
    
    5. New Notify Interface: A new Notify interface has been implemented to
       accept a dump-type parameter, enhancing the notification mechanism.
    
    Rationale:
    
    - Phosphor-Dump-Manager Role Definition: This change clearly defines
      the role of the phosphor-dump-manager for external interface
      operations like listing, deleting, and offloading dumps for
      OpenPOWER dumps. It ensures that the phosphor-dump-manager is not
      burdened with OpenPOWER-specific dump collection responsibilities,
      reducing potential impacts on the phosphor-debug-collector.
    
    - Abstraction from OpenPOWER Specifics: By handling OpenPOWER-specific
      dump collection through the openpower-dump-monitor, this change
      abstracts the phosphor-debug-collector from the complexities of
      OpenPOWER dump collection. This ensures that the phosphor-debug-
      collector focuses on communication with bmcweb and other applications
      needing to interact with dump entries.
    
    - Dump Collection Flow:
      - The phosphor-debug-collector will create dump entries in
        phosphor-dump-manager, which hosts dump manager and dump entries to
        interact with the Redfish implementation in bmcweb.
      - For OpenPOWER dumps, once the dump entry is created,
        the openpower-dump-monitor, part of openpower-debug-collector, will
        pick up the signal and create the dump.
      - Dumps collected on the BMC will store their files in a specific
        path, monitored by the openpower-dump-manager to update the entry
        status.
      - Dump entries are created with an `InProgress` status, and the
        openpower-dump-monitor will manage these entries.
      - For memory-preserving reboot dumps, the openpower-dump-monitor
        initiates a reboot, the host collects the dump, notifies the
        dump manager, and the dump manager updates the status.
      - If the dump collection failed, the openpower-dump-monitor will
        update the dump status as failed.
    
    - Commit Chain Context: This commit is part of a series aimed at
      consolidating all OpenPOWER dump management under a single manager
      at the path `/xyz/openbmc_project/dump/system` instead of
      `/xyz/openbmc_project/dump/<dumpname>`.
    
    Change-Id: I8c413e7e7ed074dc642e6c64dae6fa9a6fe35010
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>

--- dump-extensions/openpower-dumps/op_dump_consts.hpp
+++ dump-extensions/openpower-dumps/op_dump_consts.hpp
@@ -1,3 +1,10 @@
 #pragma once
 
+namespace openpower::dump
+{
+
 constexpr uint32_t INVALID_SOURCE_ID = 0xFFFFFFFF;
+constexpr auto OP_BASE_ENTRY_PATH = "/xyz/openbmc_project/dump/system/entry";
+constexpr auto OP_DUMP_OBJ_PATH = "/xyz/openbmc_project/dump/system";
+
+} // namespace openpower::dump
