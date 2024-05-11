71600c3b62e5994d700723a0aecb7a78f5f1aaa6
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2024-05-11 09:31:28 -0500

    OpenPOWER: Add notification feature to DumpEntryFactory
    This commit integrates the notification mechanism into the
    DumpEntryFactory, allowing it to handle dump for different types
    of dumps. The factory now supports creating entries dynamically
    based on notification data and can update existing entries if
    applicable. This change helps in managing different types of dumps
    (like system and resource) through a unified interface and improves
    the modularity of the OpenPOWER dump management system.
    
    The notification logic previously spread across different managers has
    been consolidated within the DumpEntryFactory.
    
    Key changes include:
    - Adding a `notifyDump` method to handle incoming notifications for
      dumps.
    - Implementing `createResourceDumpEntry` to handle specific conditions
      for resource dumps.
    - Extending the `createEntry` method to support resource dump entries
      based on the type.
    - Removal of the separate resource dump manager as its functionality is
      now integrated into the factory.
    
    Change-Id: I1d02e82ac16ebddf3dda954bef83c50f2e4976ca
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>

--- dump-extensions/openpower-dumps/op_dump_consts.hpp
+++ dump-extensions/openpower-dumps/op_dump_consts.hpp
@@ -7,4 +7,9 @@ constexpr uint32_t INVALID_SOURCE_ID = 0xFFFFFFFF;
 constexpr auto OP_BASE_ENTRY_PATH = "/xyz/openbmc_project/dump/system/entry";
 constexpr auto OP_DUMP_OBJ_PATH = "/xyz/openbmc_project/dump/system";
 
+constexpr uint32_t DUMP_ID_PREFIX_MASK = 0xF0000000;
+constexpr uint32_t DUMP_ID_DUMP_ID_MASK = 0x0FFFFFFF;
+constexpr uint32_t SYSTEM_DUMP_ID_PREFIX = 0xA0000000;
+constexpr uint32_t RESOURCE_DUMP_ID_PREFIX = 0xB0000000;
+
 } // namespace openpower::dump
