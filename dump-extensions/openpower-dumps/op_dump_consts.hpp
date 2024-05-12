254d96791e7015b6ac0d1ecdfb80d70fc5f898ae
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2024-05-11 23:14:44 -0500

    OpenPOWER: Add Hostboot dump entry support
    This commit introduces the infrastructure required to support Hostboot
    dump entry creation. The implementation includes the creation of new
    dump type Hostboot, involving modifications to the dump entry factory,
    the OpenPOWER dump manager, and associated utility functions.
    
    Key changes include:
    - Addition of Hostboot dump entry creation logic in DumpEntryFactory.
    - Enhancement of the Dump Manager to detect dump file creation.
    - Utility function to convert human-readable time to epoch time
      for dump timestamp management.
    
    Change-Id: I21c5b868865512cf44a6e3f314d27f5a8b04b40c
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>

--- dump-extensions/openpower-dumps/op_dump_consts.hpp
+++ dump-extensions/openpower-dumps/op_dump_consts.hpp
@@ -6,10 +6,13 @@ namespace openpower::dump
 constexpr uint32_t INVALID_SOURCE_ID = 0xFFFFFFFF;
 constexpr auto OP_BASE_ENTRY_PATH = "/xyz/openbmc_project/dump/system/entry";
 constexpr auto OP_DUMP_OBJ_PATH = "/xyz/openbmc_project/dump/system";
+constexpr auto OP_DUMP_PATH = "/var/lib/phosphor-debug-collector/opdump";
 
 constexpr uint32_t DUMP_ID_PREFIX_MASK = 0xF0000000;
 constexpr uint32_t DUMP_ID_DUMP_ID_MASK = 0x0FFFFFFF;
 constexpr uint32_t SYSTEM_DUMP_ID_PREFIX = 0xA0000000;
 constexpr uint32_t RESOURCE_DUMP_ID_PREFIX = 0xB0000000;
+constexpr uint32_t HARDWARE_DUMP_ID_PREFIX = 0x00000000;
+constexpr uint32_t HOSTBOOT_DUMP_ID_PREFIX = 0x20000000;
 
 } // namespace openpower::dump
