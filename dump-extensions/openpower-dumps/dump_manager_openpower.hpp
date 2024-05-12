b7a31cd6737dab4eca59230f9d8cc9cd900c5a53
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2024-05-11 23:14:44 -0500

    OpenPOWER: Add Hostboot dump entry support
    This commit introduces the infrastructure required to support Hostboot
    dump entry creation. The implementation includes the creation of new
    dump type Hostboot, involving modifications to the dump entry factory,
    the OpenPOWER dump manager, and associated utility functions.
    
    Key changes include:
    - Addition of Hostboot dump entry creation logic in DumpEntryFactory.
    - Enhancement of the Dump Manager to detect dump file creation.
    - Utility function to convert human-readable time to epoch time
      for dump timestamp management.
    
    Change-Id: I21c5b868865512cf44a6e3f314d27f5a8b04b40c
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>

--- dump-extensions/openpower-dumps/dump_manager_openpower.hpp
+++ dump-extensions/openpower-dumps/dump_manager_openpower.hpp
@@ -3,6 +3,7 @@
 #include "dump_manager.hpp"
 #include "dump_utils.hpp"
 #include "op_dump_consts.hpp"
+#include "watch.hpp"
 
 #include <com/ibm/Dump/Create/common.hpp>
 #include <com/ibm/Dump/Notify/common.hpp>
@@ -11,6 +12,7 @@
 #include <phosphor-logging/lg2.hpp>
 #include <sdbusplus/bus.hpp>
 #include <sdbusplus/server/object.hpp>
+#include <sdeventplus/source/child.hpp>
 #include <xyz/openbmc_project/Common/error.hpp>
 #include <xyz/openbmc_project/Dump/Create/server.hpp>
 
@@ -20,6 +22,11 @@ namespace openpower::dump
 using OpDumpIfaces = sdbusplus::server::object_t<
     sdbusplus::xyz::openbmc_project::Dump::server::Create,
     sdbusplus::com::ibm::Dump::server::Notify>;
+
+using UserMap = phosphor::dump::inotify::UserMap;
+
+using Watch = phosphor::dump::inotify::Watch;
+using ::sdeventplus::source::Child;
 
 using NotifyDumpTypes = sdbusplus::common::com::ibm::dump::Notify::DumpType;
 using OpDumpTypes = sdbusplus::common::com::ibm::dump::Create::DumpType;
@@ -45,11 +52,46 @@ class Manager :
      *  @param[in] event - Dump manager sd_event loop.
      *  @param[in] path - Path to attach at.
      *  @param[in] baseEntryPath - Base path of the dump entry.
+     *  @param[in] filePath The directory path where dump files are stored.
      */
-    Manager(sdbusplus::bus_t& bus, const char* path,
-            const std::string& baseEntryPath) :
+    Manager(sdbusplus::bus_t& bus, const phosphor::dump::EventPtr& event,
+            const char* path, const std::string& baseEntryPath,
+            const char* filePath) :
         OpDumpIfaces(bus, path),
-        phosphor::dump::Manager(bus, path, baseEntryPath)
+        phosphor::dump::Manager(bus, path, baseEntryPath),
+        eventLoop(event.get()),
+        dumpWatch(eventLoop, IN_NONBLOCK, IN_CLOSE_WRITE | IN_CREATE, EPOLLIN,
+                  filePath,
+                  [this](const UserMap& fileInfo) {
+        for (const auto& [path, event] : fileInfo)
+        {
+            if (event == IN_CLOSE_WRITE && !std::filesystem::is_directory(path))
+            {
+                removeWatch(path.parent_path());
+                updateEntry(path);
+            }
+            else if (event == IN_CREATE && std::filesystem::is_directory(path))
+            {
+                auto recursiveWatch = std::make_unique<Watch>(
+                    eventLoop, IN_NONBLOCK, IN_CLOSE_WRITE, EPOLLIN, path,
+                    [this](const UserMap& recursiveFileInfo) {
+                    for (const auto& [recursivePath, recursiveEvent] :
+                         recursiveFileInfo)
+                    {
+                        if (recursiveEvent == IN_CLOSE_WRITE &&
+                            !std::filesystem::is_directory(recursivePath))
+                        {
+                            removeWatch(recursivePath.parent_path());
+                            updateEntry(recursivePath);
+                        } // Here you might handle further nested directories if
+                          // needed
+                    }
+                });
+                childWatchMap.emplace(path, std::move(recursiveWatch));
+            }
+        }
+    }),
+        dumpDir(filePath)
     {}
 
     void restore() override
@@ -95,6 +137,49 @@ class Manager :
                     Argument::ARGUMENT_VALUE("INVALID INPUT"));
         }
     }
+
+  private:
+    /**
+     * @brief Removes an inotify watch from the specified path.
+     * @param[in] path The filesystem path from which to remove the watch.
+     *
+     * This method cleans up inotify watches that are no longer needed.
+     */
+    void removeWatch(const std::filesystem::path& path)
+    {
+        // Delete Watch entry from map.
+        childWatchMap.erase(path);
+    }
+
+    /**
+     * @brief Updates the dump entry based on the newly created or completed
+     * dump file.
+     * @param[in] fullPath The full path to the dump file.
+     *
+     * This method is called when a dump file is detected to be written
+     * completely. It updates the corresponding dump entry with the new file
+     * information.
+     */
+    void updateEntry(const std::filesystem::path& fullPath);
+
+    /** @brief Pointer to the event loop used for asynchronous operations.*/
+    phosphor::dump::EventPtr eventLoop;
+
+    /** @brief Inotify watch object for monitoring the dump directory.*/
+    Watch dumpWatch;
+
+    /** @brief The directory path where dump files are stored and managed.*/
+    std::string dumpDir;
+
+    /**
+     * @brief A map of child watch pointers to manage directory watches
+     * recursively.
+     *
+     * This map holds unique pointers to Watch objects for managing directory
+     * events recursively, ensuring that dumps created in nested directories are
+     * tracked.
+     */
+    std::map<std::filesystem::path, std::unique_ptr<Watch>> childWatchMap;
 };
 
 } // namespace openpower::dump
