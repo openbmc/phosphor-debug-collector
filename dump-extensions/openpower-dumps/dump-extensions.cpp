d17d3dc337ce0bad1b00be6e8356d99ecc796523
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2024-05-11 23:14:44 -0500

    OpenPOWER: Add Hostboot dump entry support
    This commit introduces the infrastructure required to support Hostboot
    dump entry creation. The implementation includes the creation of new
    dump type Hostboot, involving modifications to the dump entry factory,
    the OpenPOWER dump manager, and associated utility functions.
    
    Key changes include:
    - Addition of Hostboot dump entry creation logic in DumpEntryFactory.
    - Enhancement of the Dump Manager to detect dump file creation.
    - Utility function to convert human-readable time to epoch time
      for dump timestamp management.
    
    Change-Id: I21c5b868865512cf44a6e3f314d27f5a8b04b40c
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>

--- dump-extensions/openpower-dumps/dump-extensions.cpp
+++ dump-extensions/openpower-dumps/dump-extensions.cpp
@@ -5,17 +5,41 @@
 #include "dump-extensions/openpower-dumps/openpower_dumps_config.h"
 
 #include "dump_manager_openpower.hpp"
+#include "dump_utils.hpp"
+#include "xyz/openbmc_project/Common/error.hpp"
 
-namespace phosphor
-{
-namespace dump
+namespace phosphor::dump
 {
 
 void loadExtensions(sdbusplus::bus_t& bus, DumpManagerList& dumpList)
 {
+    using namespace phosphor::logging;
+    using InternalFailure =
+        sdbusplus::xyz::openbmc_project::Common::Error::InternalFailure;
+    sd_event* event = nullptr;
+    auto rc = sd_event_default(&event);
+    if (rc < 0)
+    {
+        lg2::error("Error occurred during the sd_event_default, rc: {RC}", "RC",
+                   rc);
+        elog<InternalFailure>();
+    }
+    EventPtr eventP{event};
+    event = nullptr;
+
+    try
+    {
+        std::filesystem::create_directories(openpower::dump::OP_DUMP_PATH);
+    }
+    catch (std::exception& e)
+    {
+        lg2::error("Failed to create OpenPOWER dump directory {PATH}", "PATH",
+                   openpower::dump::OP_DUMP_PATH);
+        throw std::runtime_error("Failed to create OpenPOWER dump directory");
+    }
+
     dumpList.push_back(std::make_unique<openpower::dump::Manager>(
-        bus, openpower::dump::OP_DUMP_OBJ_PATH,
-        openpower::dump::OP_BASE_ENTRY_PATH));
+        bus, eventP, openpower::dump::OP_DUMP_OBJ_PATH,
+        openpower::dump::OP_BASE_ENTRY_PATH, openpower::dump::OP_DUMP_PATH));
 }
-} // namespace dump
-} // namespace phosphor
+} // namespace phosphor::dump
