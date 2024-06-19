d2fae39b3afc8a4fde6810a9c05ee0014d8fc86e
Andrew Jeffery <andrew@codeconstruct.com.au>
2024-06-19 21:04:52 +0930

    host-transport-extensions: Replace deprecated libpldm header path
    There are more OEMs than IBM contributing to libpldm, so the OEM headers
    were restructured. Replace the deprecated IBM OEM header path with the
    namespaced path.
    
    The patch was generated by with the coccinelle[1] script from [2]:
    
    ```
    $ spatch \
    --sp-file .../libpldm/origin/evolutions/current/oem-ibm-header-compat.cocci \
    --in-place \
    $(git ls-files | grep -E '\.[ch](pp)?')
    ```
    
    [1]: https://coccinelle.gitlabpages.inria.fr/website/
    [2]: https://gerrit.openbmc.org/c/openbmc/libpldm/+/72202
    
    Change-Id: I60f2fb09f1f680e21522f2d3f9d9fa8ecc115bfe
    Signed-off-by: Andrew Jeffery <andrew@codeconstruct.com.au>

--- host-transport-extensions/pldm/oem/ibm/pldm_oem_cmds.cpp
+++ host-transport-extensions/pldm/oem/ibm/pldm_oem_cmds.cpp
@@ -20,7 +20,7 @@
 #include "xyz/openbmc_project/Common/error.hpp"
 
 #include <libpldm/base.h>
-#include <libpldm/file_io.h>
+#include <libpldm/oem/ibm/file_io.h>
 #include <libpldm/platform.h>
 #include <unistd.h>
 
