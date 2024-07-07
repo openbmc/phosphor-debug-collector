e5196f58a0c672497fa87de975b694dbac8d5007
Lakshmi Yadlapati <lakshmiy@us.ibm.com>
2024-07-07 18:50:30 -0500

    Add kernel MCTP (AF_MCTP) support and transport-implementation option
    -Added support for kernel MCTP (AF_MCTP) to enable MCTP communication
     using AF_MCTP sockets.
    
    - Introduced a new configuration option 'transport-implementation'
    
    The 'transport-implementation' option can be set to either:
    - 'mctp-demux': Uses the existing mctp-demux transport method.
    - 'af-mctp': Uses the new kernel AF_MCTP transport method.
    
    Change-Id: I2623de5a818bdf05614a2a38c719c572f4f0ae1e
    Signed-off-by: Lakshmi Yadlapati <lakshmiy@us.ibm.com>
    Signed-off-by: Eddie James <eajames@linux.ibm.com>

--- host-transport-extensions/pldm/common/pldm_utils.cpp
+++ host-transport-extensions/pldm/common/pldm_utils.cpp
@@ -6,11 +6,14 @@
 #include "xyz/openbmc_project/Common/error.hpp"
 
 #include <libpldm/transport.h>
+#include <libpldm/transport/af-mctp.h>
 #include <libpldm/transport/mctp-demux.h>
 #include <poll.h>
 
 #include <phosphor-logging/elog-errors.hpp>
 #include <phosphor-logging/lg2.hpp>
+
+#include <variant>
 
 namespace phosphor
 {
@@ -25,7 +28,7 @@ using Reason = xyz::openbmc_project::Common::NotAllowed::REASON;
 
 pldm_instance_db* pldmInstanceIdDb = nullptr;
 pldm_transport* pldmTransport = nullptr;
-pldm_transport_mctp_demux* mctpDemux = nullptr;
+std::variant<pldm_transport_mctp_demux*, pldm_transport_af_mctp*> impl;
 
 PLDMInstanceManager::PLDMInstanceManager()
 {
@@ -106,7 +109,17 @@ int openPLDM(mctp_eid_t eid)
         return fd;
     }
 
+#if defined(PLDM_TRANSPORT_WITH_MCTP_DEMUX)
     fd = openMctpDemuxTransport(eid);
+#elif defined(PLDM_TRANSPORT_WITH_AF_MCTP)
+    fd = openAfMctpTransport(eid);
+#else
+    lg2::error("open: No valid transport defined!");
+    elog<NotAllowed>(Reason(
+        "Required host dump action via PLDM is not allowed: "
+        "No valid transport defined!"));
+#endif
+
     if (fd < 0)
     {
         auto e = errno;
@@ -119,9 +132,11 @@ int openPLDM(mctp_eid_t eid)
     return fd;
 }
 
-int openMctpDemuxTransport(mctp_eid_t eid)
+[[maybe_unused]] int openMctpDemuxTransport(mctp_eid_t eid)
 {
+    pldm_transport_mctp_demux* mctpDemux(nullptr);
     int rc = pldm_transport_mctp_demux_init(&mctpDemux);
+    impl = mctpDemux;
     if (rc != 0)
     {
         lg2::error(
@@ -153,10 +168,52 @@ int openMctpDemuxTransport(mctp_eid_t eid)
     return pollfd.fd;
 }
 
+[[maybe_unused]] int openAfMctpTransport(mctp_eid_t eid)
+{
+    pldm_transport_af_mctp* afMctp(nullptr);
+    int rc = pldm_transport_af_mctp_init(&afMctp);
+    impl = afMctp;
+    if (rc)
+    {
+        lg2::error(
+            "openAfMctpTransport: Failed to init AF MCTP transport. rc = {RC}",
+            "RC", rc);
+        return rc;
+    }
+
+    rc = pldm_transport_af_mctp_map_tid(afMctp, eid, eid);
+    if (rc)
+    {
+        lg2::error(
+            "openAfMctpTransport: Failed to setup tid to eid mapping. rc = {RC}",
+            "RC", rc);
+        pldmClose();
+        return rc;
+    }
+    pldmTransport = pldm_transport_af_mctp_core(afMctp);
+
+    struct pollfd pollfd;
+    rc = pldm_transport_af_mctp_init_pollfd(pldmTransport, &pollfd);
+    if (rc)
+    {
+        lg2::error("openAfMctpTransport: Failed to get pollfd. rc = {RC}", "RC",
+                   rc);
+        pldmClose();
+        return rc;
+    }
+    return pollfd.fd;
+}
+
 void pldmClose()
 {
-    pldm_transport_mctp_demux_destroy(mctpDemux);
-    mctpDemux = nullptr;
+#if defined(PLDM_TRANSPORT_WITH_MCTP_DEMUX)
+    pldm_transport_mctp_demux_destroy(
+        std::get<pldm_transport_mctp_demux*>(impl));
+    impl = static_cast<pldm_transport_mctp_demux*>(nullptr);
+#elif defined(PLDM_TRANSPORT_WITH_AF_MCTP)
+    pldm_transport_af_mctp_destroy(std::get<pldm_transport_af_mctp*>(impl));
+    impl = static_cast<pldm_transport_af_mctp*>(nullptr);
+#endif
     pldmTransport = nullptr;
 }
 
