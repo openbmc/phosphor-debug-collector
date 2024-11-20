91542441e2fdf453b3be0ec74a0c30dfd0747356
SwethaParasa <parasa.swetha1@ibm.com>
2024-11-20 00:39:50 -0600

    Add utility method to create PEL
    Adding an utility method createPEL to help log PEL messages and
    createPELOnDumpActions method to create a pel whenever a dump is
    deleted or offloaded.
    
    Change-Id: I0f9a40f59c3b0e1f892e7dd83f1cbbd58c75c4c5
    Signed-off-by: SwethaParasa <parasa.swetha1@ibm.com>

--- dump_utils.hpp
+++ dump_utils.hpp
@@ -389,5 +389,33 @@ inline DumpTypes getErrorDumpType(phosphor::dump::DumpCreateParams& params)
 std::optional<std::tuple<uint32_t, uint64_t, uint64_t>>
     extractDumpDetails(const std::filesystem::path& file);
 
+/**
+ * @brief Create a new PEL message
+ *
+ * @param[in] dBus - Handle to D-Bus object
+ * @param[in] pelSev - PEL severity (Informational by default)
+ * @param[in] errIntf - D-Bus interface name.
+ * @param[in] userDataMap - Map holding the user data.
+ * @return Returns void
+ */
+void createPEL(
+    sdbusplus::bus::bus& dBus, std::string& pelSev, std::string& errIntf,
+    std::unordered_map<std::string_view, std::string_view>& userDataMap);
+/**
+ * @brief Create a new PEL message for dump Delete
+ *
+ * @param[in] dBus - Handle to D-Bus object
+ * @param[in] pelSev - PEL severity (Informational by default)
+ * @param[in] errIntf - D-Bus interface name.
+ * @param[in] dumpFilePath - Deleted dump file path/name
+ * @param[in] dumpFileType - Deleted dump file type (BMC/Resource/System)
+ * @param[in] dumpId - The dump ID
+ * @return Returns void
+ *
+ * Note: Passing by value as this has an async call
+ */
+void createPELOnDumpActions(sdbusplus::bus::bus& dBus, std::string dumpFilePath,
+                            std::string dumpFileType, std::string dumpId,
+                            std::string pelSev, std::string errIntf);
 } // namespace dump
 } // namespace phosphor
