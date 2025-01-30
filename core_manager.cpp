f10df00cf925973186f54d443fbfb2b23c7d342c
SwethaParasa <parasa.swetha1@ibm.com>
2025-01-30 02:55:57 -0600

    Retry Core Dump if dump creation failed
    When Dump manager is unavailable during core dump creation, we miss to
    create the dump. Adding a retry twice after an interval of 1 minute
    each to fix this issue.
    
    Change-Id: I296524e4f8f46cf63a91b1eb5ee3f49b8ead4177
    Signed-off-by: SwethaParasa <parasa.swetha1@ibm.com>

--- core_manager.cpp
+++ core_manager.cpp
@@ -2,6 +2,7 @@
 
 #include "core_manager.hpp"
 
+#include <unistd.h>
 #include <phosphor-logging/lg2.hpp>
 #include <sdbusplus/exception.hpp>
 
@@ -91,13 +92,20 @@ void Manager::createHelper(const vector<string>& files)
     params[DumpIntr::convertCreateParametersToString(
         CreateParameters::FilePath)] = files.front();
     m.append(params);
-    try
+    int retry = 2;
+    while (retry > 0)
     {
-        b.call_noreply(m);
-    }
-    catch (const sdbusplus::exception_t& e)
-    {
-        lg2::error("Failed to create dump: {ERROR}", "ERROR", e);
+        try
+        {
+            b.call_noreply(m);
+            retry = 0;
+        }
+        catch (const sdbusplus::exception_t& e)
+        {
+            retry--;
+            lg2::error("Failed to create dump: {ERROR}", "ERROR", e);
+            sleep(60);
+        }
     }
 }
 
