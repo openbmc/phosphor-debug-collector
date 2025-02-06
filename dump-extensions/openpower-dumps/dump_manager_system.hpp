df27c57dde1050c433b53141be1f0df0fac1166f
Jayanth Othayoth <ojayanth@gmail.com>
2025-02-06 06:06:27 -0600

    clang:openpower-dumps: fix defaulted-function-dele
    Addressed the issue where the defaulted copy constructor is implicitly
    deleted due to a base class having a deleted copy constructor.
    
    '''
    error: explicitly defaulted copy constructor is implicitly deleted [-Werror,-Wdefaulted-function-deleted]
    '''
    
    Change-Id: I4a944a9b4213142486be8e9b4bfe442dc01d1697
    Signed-off-by: Jayanth Othayoth <ojayanth@gmail.com>

--- dump-extensions/openpower-dumps/dump_manager_system.hpp
+++ dump-extensions/openpower-dumps/dump_manager_system.hpp
@@ -31,7 +31,7 @@ class Manager :
 {
   public:
     Manager() = delete;
-    Manager(const Manager&) = default;
+    Manager(const Manager&) = delete;
     Manager& operator=(const Manager&) = delete;
     Manager(Manager&&) = delete;
     Manager& operator=(Manager&&) = delete;
