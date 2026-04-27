0caf20a416631ed4ff6edad73f1ce7aa4a01b106
Patrick Williams <patrick@stwcx.xyz>
2026-04-27 05:31:28 -0400

    sdbusplus: use shorter type aliases
    The sdbusplus headers provide shortened aliases for many types.
    Switch to using them to provide better code clarity and shorter
    lines.  Possible replacements are for:
      * bus_t
      * exception_t
      * manager_t
      * match_t
      * message_t
      * object_t
      * slot_t
      * object_path
    
    Change-Id: Ied42bad18912ebc1e2da9d6cba5ba629d8697064
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- dump-extensions/openpower-dumps/dump_manager_system.hpp
+++ dump-extensions/openpower-dumps/dump_manager_system.hpp
@@ -67,7 +67,7 @@ class Manager :
      *
      *  @return object_path - The path to the new dump entry.
      */
-    sdbusplus::message::object_path createDump(
+    sdbusplus::object_path createDump(
         phosphor::dump::DumpCreateParams params) override;
 };
 
